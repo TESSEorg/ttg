98e517c9a180bc1a0919ecc6b059b90f50bf025b
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-09-23 15:26:50 -0400

    amended 23c8b4027faafd2debb2853363d12515f17e8af5

--- parsec/ttg.h
+++ parsec/ttg.h
@@ -416,7 +416,8 @@ class Op : public ::ttg::OpBase, ParsecBaseOp {
     assert(task->user_tuple != nullptr);
     input_values_tuple_type *tuple = static_cast<input_values_tuple_type *>(task->user_tuple);
 
-    new (&std::get<i>(*tuple)) data_wrapper_t<valueT>(wrap(std::forward<T>(value)));
+    // is value is an actual value rather than wrapper (pointer) need in wrap distinguish between data held by datacopy and data not owned by PaRSEC
+    std::get<i>(*tuple) = data_wrapper_t<valueT>(wrap(std::forward<T>(value)));
     parsec_data_copy_t* copy = OBJ_NEW(parsec_data_copy_t);
     task->parsec_task.data[i].data_in = copy;
     copy->device_private = (void*)(std::get<i>(*tuple));  // tuple holds pointers already
