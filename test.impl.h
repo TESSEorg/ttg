5c96ea0aaaa446184906f11e0f02a3b1e5b9d08a
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-09-24 18:35:05 -0400

    sanity check is converted to runtime-agnostic form ... except wrap tests since not clear best way to do this (force wrapped functions to take data as a tuple of wrappers or convert tuple of wrappers to tuple of values)

--- test.impl.h
+++ test.impl.h
@@ -5,81 +5,75 @@ using keyT = uint64_t;
 #include TTG_RUNTIME_H
 IMPORT_TTG_RUNTIME_NS
 
-class A : public Op<keyT, std::tuple<Out<keyT, int>, Out<keyT, int>>,
-                       A, int> {
-  using baseT =
-      Op<keyT, std::tuple<Out<keyT, int>, Out<keyT, int>>, A, int>;
+class A : public Op<keyT, std::tuple<Out < keyT, int>, Out < keyT, int>>
+, A, int> {
+using baseT = Op<keyT, std::tuple<Out < keyT, int>, Out < keyT, int>>, A, int>;
 
- public:
-  A(const std::string& name) : baseT(name, {"input"}, {"result", "iterate"}) {}
+public:
+A(const std::string &name) : baseT(name, {"input"}, {"iterate", "result"}) {}
 
-  A(const typename baseT::input_edges_type& inedges,
-    const typename baseT::output_edges_type& outedges, const std::string& name)
-      : baseT(inedges, outedges, name, {"input"}, {"result", "iterate"}) {}
+A(const typename baseT::input_edges_type &inedges, const typename baseT::output_edges_type &outedges,
+  const std::string &name)
+    : baseT(inedges, outedges, name, {"input"}, {"result", "iterate"}) {}
 
-  void op(const keyT& key, baseT::input_values_tuple_type&& t,
-          baseT::output_terminals_type& out) {
-    int value = baseT::get<0>(t);
-    std::cout << "A got value " << value << std::endl;
-    if (value >= 100) {
-      ::send<0>(key, value, out);
-    } else {
-      ::send<1>(key + 1, value + 1, out);
-    }
+void op(const keyT &key, baseT::input_values_tuple_type &&t, baseT::output_terminals_type &out) {
+  int value = baseT::get<0>(t);
+  ::ttg::print("A got value ", value);
+  if (value >= 100) {
+    ::send<0>(key, value, out);
+  } else {
+    ::send<1>(key + 1, value + 1, out);
   }
+}
+
+~A() { std::cout << " A destructor\n"; }
 };
 
-class Producer : public Op<keyT, std::tuple<Out<keyT, int>>, Producer> {
-  using baseT = Op<keyT, std::tuple<Out<keyT, int>>, Producer>;
+class Producer : public Op<keyT, std::tuple<Out < keyT, int>>, Producer
+> {
+using baseT = Op<keyT, std::tuple<Out < keyT, int>>, Producer>;
 
- public:
-  Producer(const std::string& name) : baseT(name, {}, {"output"}) {}
+public:
+Producer(const std::string &name) : baseT(name, {}, {"output"}) {}
 
-  Producer(const typename baseT::output_edges_type& outedges,
-           const std::string& name)
-      : baseT(edges(), outedges, name, {}, {"output"}) {}
+Producer(const typename baseT::output_edges_type &outedges, const std::string &name)
+    : baseT(edges(), outedges, name, {}, {"output"}) {}
 
-  void op(const keyT& key, baseT::input_values_tuple_type&& t,
-          baseT::output_terminals_type& out) {
-    std::cout << "produced " << 0 << std::endl;
-    ::send<0>((int)(key), 0, out);
-  }
+void op(const keyT &key, baseT::input_values_tuple_type &&t, baseT::output_terminals_type &out) {
+  ::ttg::print("produced ", 0);
+  ::send<0>((int) (key), 0, out);
+}
+
+~Producer() { std::cout << " Producer destructor\n"; }
 };
 
 class Consumer : public Op<keyT, std::tuple<>, Consumer, int> {
   using baseT = Op<keyT, std::tuple<>, Consumer, int>;
 
  public:
-  Consumer(const std::string& name) : baseT(name, {"input"}, {}) {}
-  void op(const keyT& key, baseT::input_values_tuple_type&& t,
-          baseT::output_terminals_type& out) {
-    std::cout << "consumed " << baseT::get<0>(t) << std::endl;
+  Consumer(const std::string &name) : baseT(name, {"input"}, {}) {}
+  void op(const keyT &key, baseT::input_values_tuple_type &&t, baseT::output_terminals_type &out) {
+    ::ttg::print("consumed ", baseT::get<0>(t));
   }
 
-  Consumer(const typename baseT::input_edges_type& inedges,
-           const std::string& name)
+  Consumer(const typename baseT::input_edges_type &inedges, const std::string &name)
       : baseT(inedges, edges(), name, {"input"}, {}) {}
+
+  ~Consumer() { std::cout << " Consumer destructor\n"; }
 };
 
-class Everything : public Op<keyT, std::tuple<>, Everything> {
-  using baseT = Op<keyT, std::tuple<>, Everything>;
-
+class Everything {
   Producer producer;
   A a;
   Consumer consumer;
 
  public:
-  Everything()
-      : baseT("everything", {}, {}),
-        producer("producer"),
-        a("A"),
-        consumer("consumer") {
-      connect<0,0>(&producer, &a);
-      connect<0,0>(&a, &consumer);
-      connect<1,0>(&a, &a);
+  Everything() : producer("producer"), a("A"), consumer("consumer") {
+    connect<0, 0>(&producer, &a);  // producer.out<0>()->connect(a.in<0>());
+    connect<0, 0>(&a, &consumer);  // a.out<0>()->connect(consumer.in<0>());
+    connect<1, 0>(&a, &a);         // a.out<1>()->connect(a.in<0>());
 
-      Verify()(&producer);
-      // ctx->fence();
+    Verify()(&producer);
   }
 
   void print() { Print()(&producer); }
@@ -87,172 +81,254 @@ class Everything : public Op<keyT, std::tuple<>, Everything> {
   std::string dot() { return Dot()(&producer); }
 
   void start() {
-    // if (my rank = 0)
     producer.invoke(0);
   }
 };
 
+class EverythingBase {
+  std::unique_ptr<OpBase> producer;
+  std::unique_ptr<OpBase> a;
+  std::unique_ptr<OpBase> consumer;
+
+ public:
+  EverythingBase()
+      : producer(new Producer("producer")), a(new A("A")), consumer(new Consumer("consumer")) {
+    connect<0, 0>(producer, a);  // producer->out(0)->connect(a->in(0));
+    connect<0, 0>(a, consumer);  // a->out(0)->connect(consumer->in(0));
+    connect<1, 0>(a, a);         // a->out(1)->connect(a->in(0));
+
+    Verify()(producer.get());
+  }
+
+  void print() { Print()(producer.get()); }
+
+  std::string dot() { return Dot()(producer.get()); }
+
+  void start() {
+    if (ttg_default_execution_context().rank() == 0) dynamic_cast<Producer *>(producer.get())->invoke(0);
+  }  // Ugh!
+};
+
+class Everything2 {
+  Edge<keyT, int> P2A, A2A, A2C;  // !!!! Edges must be constructed before classes that use them
+  Producer producer;
+  A a;
+  Consumer consumer;
+
+ public:
+  Everything2()
+      : P2A("P2A"),
+        A2A("A2A"),
+        A2C("A2C"),
+        producer(edges(P2A), "producer"),
+        a(edges(fuse(P2A, A2A)), edges(A2C, A2A), "A"),
+        consumer(edges(A2C), "consumer") {
+  }
+
+  void print() { Print()(&producer); }
+
+  std::string dot() { return Dot()(&producer); }
+
+  void start() {
+    if (ttg_default_execution_context().rank() == 0) producer.invoke(0);
+  }  // Ugh!
+};
+
 #if 0
-class Everything2 : public Op<keyT, std::tuple<>, Everything2> {
-    using baseT =          Op<keyT, std::tuple<>, Everything2>;
-    
-    Edge<keyT,int> P2A, A2A, A2C; // !!!! Edges must be constructed before classes that use them
-    Producer producer;
-    A a;
-    Consumer consumer;
-
-    World& world;
-public:
-    Everything2()
-        : baseT("everything", {}, {})
-        , P2A("P2A"), A2A("A2A"), A2C("A2C")
-        , producer(P2A, "producer")
-        , a(fuse(P2A,A2A), edges(A2C,A2A), "A")
-        , consumer(A2C, "consumer")
-        , world(madness::World::get_default())
-    {
-        world.gop.fence();
-    }
-    
-    void print() {Print()(&producer);}
-
-    std::string dot() {return Dot()(&producer);}
-    
-    void start() {if (world.rank() == 0) producer.invoke(0);}
-    
-    void wait() {world.gop.fence();}
-};
-
 class Everything3 {
+  static void p(const keyT& key, std::tuple<>&& t, std::tuple<Out<keyT, int>>& out) {
+    ::ttg::print("produced ", 0);
+    send<0>(key, int(key), out);
+  }
 
-    static void p(const keyT& key, const std::tuple<>& t, std::tuple<Out<keyT,int>>& out) {
-        std::cout << "produced " << 0 << std::endl;
-        send<0>(key,int(key),out);
+  static void a(const keyT& key, std::tuple<int>&& t, std::tuple<Out<keyT, int>, Out<keyT, int>>& out) {
+    int value = std::get<0>(t);
+    if (value >= 100) {
+      send<0>(key, value, out);
+    } else {
+      send<1>(key + 1, value + 1, out);
     }
+  }
 
-    static void a(const keyT& key, const std::tuple<int>& t, std::tuple<Out<keyT,int>,Out<keyT,int>>&  out) {
-        int value = std::get<0>(t);
-        if (value >= 100) {
-            send<0>(key, value, out);
-        }
-        else {
-            send<1>(key+1, value+1, out);
-        }
-    }
+  static void c(const keyT& key, std::tuple<int>&& t, std::tuple<>& out) {
+    ::ttg::print("consumed ", std::get<0>(t));
+  }
 
-    static void c(const keyT& key, const std::tuple<int>& t, std::tuple<>& out) {
-        std::cout << "consumed " << std::get<0>(t) << std::endl;
-    }
+  Edge<keyT, int> P2A, A2A, A2C;  // !!!! Edges must be constructed before classes that use them
 
-    Edge<keyT,int> P2A, A2A, A2C; // !!!! Edges must be constructed before classes that use them
+  decltype(wrapt<keyT>(&p, edges(), edges(P2A))) wp;
+  decltype(wrapt(&a, edges(fuse(P2A, A2A)), edges(A2C, A2A))) wa;
+  decltype(wrapt(&c, edges(A2C), edges())) wc;
 
-    decltype(wrapt<keyT>(&p, edges(), edges(P2A))) wp;
-    decltype(wrapt(&a, edges(fuse(P2A,A2A)), edges(A2C,A2A))) wa;
-    decltype(wrapt(&c, edges(A2C), edges())) wc;
+ public:
+  Everything3()
+      : P2A("P2A")
+      , A2A("A2A")
+      , A2C("A2C")
+      , wp(wrapt<keyT>(&p, edges(), edges(P2A), "producer", {}, {"start"}))
+      , wa(wrapt(&a, edges(fuse(P2A, A2A)), edges(A2C, A2A), "A", {"input"}, {"result", "iterate"}))
+      , wc(wrapt(&c, edges(A2C), edges(), "consumer", {"result"}, {})) {
+  }
 
-public:
-    Everything3()
-        : P2A("P2A"), A2A("A2A"), A2C("A2C")
-        , wp(wrapt<keyT>(&p, edges(), edges(P2A), "producer",{},{"start"}))
-        , wa(wrapt(&a, edges(fuse(P2A,A2A)), edges(A2C,A2A), "A",{"input"},{"result","iterate"}))
-        , wc(wrapt(&c, edges(A2C), edges(), "consumer",{"result"},{}))
-    {
-        madness::World::get_default().gop.fence();
-    }
-    
-    void print() {Print()(wp.get());}
+  void print() { Print()(wp.get()); }
 
-    std::string dot() {return Dot()(wp.get());}
-    
-    void start() {if (madness::World::get_default().rank() == 0) wp->invoke(0);}
-    
-    void wait() {madness::World::get_default().gop.fence();}
+  std::string dot() { return Dot()(wp.get()); }
+
+  void start() {
+    if (ttg_default_execution_context().rank() == 0) wp->invoke(0);
+  }
 };
-    
+
 class Everything4 {
+  static void p(const keyT& key, std::tuple<Out<keyT, int>>& out) {
+    ::ttg::print("produced ", 0);
+    send<0>(key, int(key), out);
+  }
 
-    static void p(const keyT& key, std::tuple<Out<keyT,int>>& out) {
-        std::cout << "produced " << 0 << std::endl;
-        send<0>(key,int(key),out);
+  static void a(const keyT& key, int&& value, std::tuple<Out<keyT, int>, Out<keyT, int>>& out) {
+    if (value >= 100) {
+      send<0>(key, value, out);
+    } else {
+      send<1>(key + 1, value + 1, out);
     }
+  }
 
-    static void a(const keyT& key, int value, std::tuple<Out<keyT,int>,Out<keyT,int>>&  out) {
-        if (value >= 100) {
-            send<0>(key, value, out);
-        }
-        else {
-            send<1>(key+1, value+1, out);
-        }
-    }
+  static void c(const keyT& key, int&& value, std::tuple<>& out) {
+    ::ttg::print("consumed ", value);
+  }
 
-    static void c(const keyT& key, int value, std::tuple<>& out) {
-        std::cout << "consumed " << value << std::endl;
-    }
+  Edge<keyT, int> P2A, A2A, A2C;  // !!!! Edges must be constructed before classes that use them
 
-    Edge<keyT,int> P2A, A2A, A2C; // !!!! Edges must be constructed before classes that use them
+  decltype(wrap<keyT>(&p, edges(), edges(P2A))) wp;
+  decltype(wrap(&a, edges(fuse(P2A, A2A)), edges(A2C, A2A))) wa;
+  decltype(wrap(&c, edges(A2C), edges())) wc;
 
-    decltype(wrap<keyT>(&p, edges(), edges(P2A))) wp;
-    decltype(wrap(&a, edges(fuse(P2A,A2A)), edges(A2C,A2A))) wa;
-    decltype(wrap(&c, edges(A2C), edges())) wc;
+ public:
+  Everything4()
+      : P2A("P2A")
+      , A2A("A2A")
+      , A2C("A2C")
+      , wp(wrap<keyT>(&p, edges(), edges(P2A), "producer", {}, {"start"}))
+      , wa(wrap(&a, edges(fuse(P2A, A2A)), edges(A2C, A2A), "A", {"input"}, {"result", "iterate"}))
+      , wc(wrap(&c, edges(A2C), edges(), "consumer", {"result"}, {})) {
+  }
 
-public:
-    Everything4()
-        : P2A("P2A"), A2A("A2A"), A2C("A2C")
-        , wp(wrap<keyT>(&p, edges(), edges(P2A), "producer",{},{"start"}))
-        , wa(wrap(&a, edges(fuse(P2A,A2A)), edges(A2C,A2A), "A",{"input"},{"result","iterate"}))
-        , wc(wrap(&c, edges(A2C), edges(), "consumer",{"result"},{}))
-    {
-        madness::World::get_default().gop.fence();
-    }
-    
-    void print() {Print()(wp.get());}
+  void print() { Print()(wp.get()); }
 
-    std::string dot() {return Dot()(wp.get());}
-    
-    void start() {if (madness::World::get_default().rank() == 0) wp->invoke(0);}
-    
-    void wait() {madness::World::get_default().gop.fence();}
+  std::string dot() { return Dot()(wp.get()); }
+
+  void start() {
+    if (ttg_default_execution_context().rank() == 0) wp->invoke(0);
+  }
 };
 #endif
 
-int main(int argc, char** argv) {
+class EverythingComposite {
+  std::unique_ptr<OpBase> P;
+  std::unique_ptr<OpBase> AC;
+
+ public:
+  EverythingComposite() {
+    auto p = std::make_unique<Producer>("P");
+    auto a = std::make_unique<A>("A");
+    auto c = std::make_unique<Consumer>("C");
+
+    ::ttg::print("P out<0>", (void *) (TerminalBase *) (p->out<0>()));
+    ::ttg::print("A  in<0>", (void *) (TerminalBase *) (a->in<0>()));
+    ::ttg::print("A out<0>", (void *) (TerminalBase *) (a->out<0>()));
+    ::ttg::print("C  in<0>", (void *) (TerminalBase *) (c->in<0>()));
+
+    connect<1, 0>(a, a);  // a->out<1>()->connect(a->in<0>());
+    connect<0, 0>(a, c);  // a->out<0>()->connect(c->in<0>());
+    std::tuple<In<keyT, int> *> q = std::make_tuple(a->in<0>());
+    ::ttg::print("q  in<0>", (void *) (TerminalBase *) (std::get<0>(q)));
+
+    // std::array<std::unique_ptr<OpBase>,2> ops{std::move(a),std::move(c)};
+    std::vector<std::unique_ptr<OpBase>> ops(2);
+    ops[0] = std::move(a);
+    ops[1] = std::move(c);
+    ::ttg::print("opsin(0)", (void *) (ops[0]->in(0)));
+    ::ttg::print("ops size", ops.size());
+
+    auto ac = make_composite_op(std::move(ops), q, std::make_tuple(), "Fred");
+
+    ::ttg::print("AC in<0>", (void *) (TerminalBase *) (ac->in<0>()));
+    connect<0, 0>(p, ac);  // p->out<0>()->connect(ac->in<0>());
+
+    Verify()(p.get());
+
+    P = std::move(p);
+    AC = std::move(ac);
+  }
+
+  void print() { Print()(P.get()); }
+
+  std::string dot() { return Dot()(P.get()); }
+
+  void start() {
+    Producer * p = dynamic_cast<Producer *>(P.get());
+    p->invoke(0);
+  }  // Ugh!
+};
+
+void hi() { ::ttg::print("hi"); }
+
+int main(int argc, char **argv) {
   ttg_initialize(argc, argv, 2);
   {
-    OpBase::set_trace_all(true);
+
+    OpBase::set_trace_all(false);
+
+    ttg_execute(ttg_default_execution_context());
 
     // First compose with manual classes and connections
     Everything x;
     x.print();
     std::cout << x.dot() << std::endl;
 
-    x.start();
+    x.start();  // myusleep(100);
 
-    ttg_execute(ttg_default_execution_context());
-    ttg_fence(ttg_default_execution_context());
+    // Next compose with base class pointers and verify destruction
+    EverythingBase x1;
+    x1.print();
+    std::cout << x1.dot() << std::endl;
 
-#if 0
+    x1.start();  // myusleep(100);
+
+    // Now try with the composite operator wrapping A and C
+    EverythingComposite x2;
+
+    std::cout << "\nComposite\n";
+    std::cout << x2.dot() << std::endl << std::endl;
+    x2.start();  // myusleep(100);
+    std::cout << "\nComposite done\n";
+
     // Next compose with manual classes and edges
     Everything2 y;
     y.print();
     std::cout << y.dot() << std::endl;
-    
-    y.start();
-    y.wait();
 
+    y.start();  // myusleep(100);
+
+#if 0
     // Next compose with wrappers using tuple API and edges
-    Everything3 z;
-    std::cout << z.dot() << std::endl;
-    z.start();
-    z.wait();
-    
+    {
+      Everything3 z;
+      std::cout << z.dot() << std::endl;
+      z.start();  // myusleep(100);
+    }
+
     // Next compose with wrappers using unpacked tuple API and edges
-    Everything4 q;
-    std::cout << q.dot() << std::endl;
-    q.start();
-    q.wait();
+    {
+      Everything4 q;
+      std::cout << q.dot() << std::endl;
+      q.start();  // myusleep(100);
+    }
 #endif
+    ttg_fence(ttg_default_execution_context());
+
   }
   ttg_finalize();
-
   return 0;
 }
+
