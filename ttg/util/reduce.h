7faefc9652b4f2f9339c06db6d0c5b5acf0cd9bc
Eduard Valeyev <eduard@valeyev.net>
2017-12-29 09:33:40 -0500

    factored out the binary tree from reduction

--- ttg/util/reduce.h
+++ ttg/util/reduce.h
@@ -9,114 +9,96 @@
 #include <cstdlib>
 #include <mutex>
 
-/// @brief generic binary tree reduction operation over key-value pairs.
-///
-/// This reduces a set of {Key,Value} pairs using BinaryOp
-/// This reduces one value for each key, using a binary spanning tree rooted at a particular key.
-/// The primary use is for reducing over a World, hence the default keymap is identity (keymap(key) = key).
-///
-/// @note this is equivalent to MPI_Reduce; see also std::reduce for more info
-///
-template <typename Value, typename BinaryOp, typename OutKey>
-class BinaryTreeReduce
-    : public Op<int, std::tuple<Out<int, Value>, Out<int, Value>, Out<int, Value>, Out<OutKey, Value>>,
-                BinaryTreeReduce<Value, BinaryOp, OutKey>, Value, Value, Value> {
- public:
-  using baseT = Op<int, std::tuple<Out<int, Value>, Out<int, Value>, Out<int, Value>, Out<OutKey, Value>>,
-                   BinaryTreeReduce<Value, BinaryOp, OutKey>, Value, Value, Value>;
+#include "../../ttg/util/tree.h"
 
-  BinaryTreeReduce(Edge<int, Value> &in, Edge<OutKey, Value> &out, int root = 0, OutKey dest_key = OutKey(),
-                   BinaryOp op = BinaryOp{}, World &world = ttg_default_execution_context(),
-                   Edge<int, Value> inout = Edge<int, Value>{}, Edge<int, Value> inout_l = Edge<int, Value>{}, Edge<int, Value> inout_r = Edge<int, Value>{})
-      : baseT(edges(fuse(in, inout), inout_l, inout_r), edges(inout, inout_l, inout_r, out), "BinaryTreeReduce",
-              {"in+inout", "inout_l", "inout_r"}, {"inout", "inout_l", "inout_r", "out"}, world)
-      , max_key_(world.size())
-      , root_(root)
-      , dest_key_(dest_key)
-      , op_(std::move(op)) {
-    assert(root_ < max_key_);
-    init();
-  }
+namespace ttg {
 
-  void op(const int &key, typename baseT::input_values_tuple_type &&indata,
-          std::tuple<Out<int, Value>, Out<int, Value>, Out<int, Value>, Out<OutKey, Value>> &outdata) {
-    assert(key < max_key_);
-    /// skip stub values ... won't need this ugliness when streaming is implemented
-    const auto my_key = this->get_world().rank();
-    auto children = child_keys(my_key);
-    Value result;
-    if (children.first != -1 && children.second != -1)
-      // L op This op R
-      result = op_(op_(baseT::template get<0, Value &&>(indata), baseT::template get<1, Value &&>(indata)),baseT::template get<2, Value &&>(indata));
-    else {
-      if (children.first != -1)
-        result = op_(baseT::template get<0, Value &&>(indata), baseT::template get<1, Value &&>(indata));
-      else if (children.second != -1)
-        result = op_(baseT::template get<0, Value &&>(indata), baseT::template get<2, Value &&>(indata));
-      else
-        result = baseT::template get<0, Value &&>(indata);
+  /// @brief generic binary reduction of a set of key-value pairs.
+  ///
+  /// This reduces a set of Value objects keyed by an integer in the @c [0,max_key) interval using BinaryOp @c op .
+  /// The reduction order is determined by breadth-first traversal of a
+  /// binary spanning tree of the complete graph of the @c [0,max_key) set (see BinarySpanningTree)
+  /// rooted at a particular key; at each node @c Node the reduction is performed
+  /// as @c op(op(LeftSubTree,Node),RightSubTree) .
+  /// The primary use is for reducing over a World, hence by default the keymap is identity (keymap(key) = key) and
+  /// @c max_key=world.size() . The result is associated with output key @c dest_key .
+  ///
+  /// @note this is equivalent to MPI_Reduce; unlike std::reduce this lacks the initializer value.
+  ///
+  template <typename Value, typename BinaryOp, typename OutKey>
+  class BinaryTreeReduce
+      : public Op<int, std::tuple<Out<int, Value>, Out<int, Value>, Out<int, Value>, Out<OutKey, Value>>,
+                  BinaryTreeReduce<Value, BinaryOp, OutKey>, Value, Value, Value> {
+   public:
+    using baseT = Op<int, std::tuple<Out<int, Value>, Out<int, Value>, Out<int, Value>, Out<OutKey, Value>>,
+                     BinaryTreeReduce<Value, BinaryOp, OutKey>, Value, Value, Value>;
+
+    BinaryTreeReduce(Edge<int, Value> &in, Edge<OutKey, Value> &out, int root = 0, OutKey dest_key = OutKey(),
+                     BinaryOp op = BinaryOp{}, World &world = ttg_default_execution_context(), int max_key = -1,
+                     Edge<int, Value> inout = Edge<int, Value>{}, Edge<int, Value> inout_l = Edge<int, Value>{},
+                     Edge<int, Value> inout_r = Edge<int, Value>{})
+        : baseT(edges(fuse(in, inout), inout_l, inout_r), edges(inout, inout_l, inout_r, out), "BinaryTreeReduce",
+                {"in|inout", "inout_l", "inout_r"}, {"inout", "inout_l", "inout_r", "out"}, world)
+        , tree_((max_key == -1 ? world.size() : max_key), root)
+        , dest_key_(dest_key)
+        , op_(std::move(op)) {
+      init();
     }
-    auto parent = parent_key(my_key);
-    if (parent != -1) {
-      // is this left or right child of the parent?
-      bool this_is_left_child;
-      {
-        auto parents_children = child_keys(parent);
-        assert(parents_children.first == my_key || parents_children.second == my_key);
-        this_is_left_child = (parents_children.first == my_key);
+
+    void op(const int &key, typename baseT::input_values_tuple_type &&indata,
+            std::tuple<Out<int, Value>, Out<int, Value>, Out<int, Value>, Out<OutKey, Value>> &outdata) {
+      assert(key < tree_.size());
+      /// skip stub values ... won't need this ugliness when streaming is implemented
+      auto children = tree_.child_keys(key);
+      Value result;
+      if (children.first != -1 && children.second != -1)
+        // left-associative in-order reduction: L op This op R = ((L op This) op R)
+        result = op_(op_(baseT::template get<1, Value &&>(indata), baseT::template get<0, Value &&>(indata)),
+                     baseT::template get<2, Value &&>(indata));
+      else {
+        if (children.first != -1)
+          result = op_(baseT::template get<1, Value &&>(indata), baseT::template get<0, Value &&>(indata));
+        else if (children.second != -1)
+          result = op_(baseT::template get<0, Value &&>(indata), baseT::template get<2, Value &&>(indata));
+        else
+          result = baseT::template get<0, Value &&>(indata);
       }
-      if (this_is_left_child)
-        send<1>(parent, std::move(result), outdata);
-      else
-        send<2>(parent, std::move(result), outdata);
+      auto parent = tree_.parent_key(key);
+      if (parent != -1) {
+        // is this left or right child of the parent?
+        bool this_is_left_child;
+        {
+          auto parents_children = tree_.child_keys(parent);
+          assert(parents_children.first == key || parents_children.second == key);
+          this_is_left_child = (parents_children.first == key);
+        }
+        if (this_is_left_child)
+          send<1>(parent, std::move(result), outdata);
+        else
+          send<2>(parent, std::move(result), outdata);
+      } else
+        send<3>(dest_key_, std::move(result), outdata);
     }
-    else
-      send<3>(dest_key_, std::move(result), outdata);
-  }
 
- private:
-  int max_key_;
-  int root_;
-  OutKey dest_key_;
-  BinaryOp op_;
+   private:
+    BinarySpanningTree tree_;
+    OutKey dest_key_;
+    BinaryOp op_;
 
-  /// since the # of arguments is constexpr in current TTG, some reductions will use stub values, initialize them here
-  /// TODO this will initialize stream bounds when TTG supports streaming
-  void init() {
-    const auto my_key = this->get_world().rank();
-    auto keys = child_keys(my_key);
-    if (keys.first == -1) this->template set_arg<1>(my_key, Value());
-    if (keys.second == -1) this->template set_arg<2>(my_key, Value());
-  }
-
-  /// @param[in] child_key the key of the child
-  /// @return the parent key (-1 if there is no parent)
-  int parent_key(const int child_key) const {
-    const auto child_rank =
-        (child_key + max_key_ - root_) % max_key_;  // cyclically shifted key such that root's key is 0
-    const auto parent_key =
-        (child_rank == 0 ? -1 : (((child_rank - 1) >> 1) + root_) % max_key_);  // Parent's key in binary tree
-    return parent_key;
-  }
-  /// @param[in] parent_key the key of the parent
-  /// @return the pair of child keys (-1 if there is no child)
-  std::pair<int, int> child_keys(const int parent_key) const {
-    const auto parent_rank =
-        (parent_key + max_key_ - root_) % max_key_;  // cyclically shifted key such that root's key is 0
-    int child0 = (parent_rank << 1) + 1 + root_;     // Left child
-    int child1 = child0 + 1;                         // Right child
-    const int max_key_plus_root = max_key_ + root_;
-    if (child0 < max_key_plus_root)
-      child0 %= max_key_;
-    else
-      child0 = -1;
-    if (child1 < max_key_plus_root)
-      child1 %= max_key_;
-    else
-      child1 = -1;
-    return std::make_pair(child0, child1);
-  }
-};
+    /// since the # of arguments is constexpr in current TTG, some reductions will use stub values, initialize them here
+    /// TODO this will initialize stream bounds when TTG supports streaming
+    void init() {
+      // iterate over keys that map to me ... if keys are equivalent to ranks this can be made simpler
+      const auto my_rank = this->get_world().rank();
+      for (auto key = 0; key != tree_.size(); ++key) {
+        if (my_rank == this->get_keymap()(key)) {
+          auto keys = tree_.child_keys(key);
+          if (keys.first == -1) this->template set_arg<1>(key, Value());
+          if (keys.second == -1) this->template set_arg<2>(key, Value());
+        }
+      }
+    }
+  };
 
 #if 0
 /// @brief generic reduction operation
@@ -153,5 +135,7 @@ class Reduce : public Op<InKey, std::tuple<Out<OutKey, Value>>, Reduce<InKey, Va
   Reducer reducer_;
 };  // class Reduce
 #endif
+
+}  // namespace ttg
 
 #endif  // TTG_REDUCE_H
---
+++ ttg/util/tree.h
@@ -0,0 +1,65 @@
+//
+// Created by Eduard Valeyev on 12/29/17.
+//
+
+#ifndef TTG_TREE_H
+#define TTG_TREE_H
+
+#include <cassert>
+#include <utility>
+
+namespace ttg {
+
+  /// @brief a binary spanning tree of integers in the @c [0,size) interval
+  ///
+  /// This is a binary spanning tree of the complete graph of the @c [0,size) set of <em>keys</em>,
+  /// rooted at a particular key.
+  class BinarySpanningTree {
+   public:
+    BinarySpanningTree(int size, int root) : size_(size), root_(root) {
+      assert(root >= 0 && root < size);
+      assert(size >= 0);
+    }
+    ~BinarySpanningTree() = default;
+
+    /// @return the size of the tree
+    const auto size() const { return size_; }
+    /// @return the root of the tree
+    const auto root() const { return root_; }
+
+    /// @param[in] child_key the key of the child
+    /// @return the parent key (-1 if there is no parent)
+    int parent_key(const int child_key) const {
+      const auto child_rank =
+          (child_key + size_ - root_) % size_;  // cyclically shifted key such that root's key is 0
+      const auto parent_key =
+          (child_rank == 0 ? -1 : (((child_rank - 1) >> 1) + root_) % size_);  // Parent's key in binary tree
+      return parent_key;
+    }
+    /// @param[in] parent_key the key of the parent
+    /// @return the pair of child keys (-1 if there is no child)
+    std::pair<int, int> child_keys(const int parent_key) const {
+      const auto parent_rank =
+          (parent_key + size_ - root_) % size_;  // cyclically shifted key such that root's key is 0
+      int child0 = (parent_rank << 1) + 1 + root_;     // Left child
+      int child1 = child0 + 1;                         // Right child
+      const int size_plus_root = size_ + root_;
+      if (child0 < size_plus_root)
+        child0 %= size_;
+      else
+        child0 = -1;
+      if (child1 < size_plus_root)
+        child1 %= size_;
+      else
+        child1 = -1;
+      return std::make_pair(child0, child1);
+    }
+
+   private:
+    int size_;
+    int root_;
+  };
+
+}  // namespace ttg
+
+#endif  // TTG_TREE_H
