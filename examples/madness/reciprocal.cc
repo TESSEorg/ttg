0fcddc6179f233912f317fbd0f9a17ee6626284e
Robert J. Harrison <rjharrison@gmail.com>
2018-03-20 21:41:40 -0400

    example now working end to end using crude sum and crude broadcast

--- examples/madness/reciprocal.cc
+++ examples/madness/reciprocal.cc
@@ -10,9 +10,56 @@ using namespace ::ttg;
 // APPLICATION STUFF BELOW
 #include <cmath>
 #include <utility>
+#include <cassert>
+#include <cstdlib>
 #include <iostream>
+#include <functional>
 
-// Iteration to compute y=1/x .... y <-- y*(2-x*y)
+// Iteration to compute y=1/x .... y <-- y*(2-x*y) ... for a vector of numbers with 1 task per vector element
+
+/***
+
+     ** design choice: if an Op is used in phases so that it is never
+     ** possible for the same key to arrive at the same time it is
+     ** permissible to reuse keys in op playing the same algorithmic role on
+     ** successive iterations
+     ** 
+     ** e.g., a reduce operation in an iterative solve --- since it is
+     ** a syncrhonization point it is not possible for multiple identical keys to
+     ** arrive at the same time
+     ** 
+     ** e.g., instead of key being pair(iteration,vector-index) it
+     ** suffices to just use key=vector-index.
+     **
+     ** this can fail at runtime in two ways --- multiple assignment
+     ** (runtime detection) or left over tasks (runtime detection) or
+     ** wrong numbers
+     **
+     ** This is essential to avoid the key potentially becoming the
+     ** union of all keys within an algorithm.  Example here is
+     ** iteration index being joined to summation index in inner loop.
+
+     ** issue: x logically depends only on i, but needs to be forwarded to all iterations k --- must we do this explicitly?
+
+     ** easiest is to send x along with y ... change made
+
+     ** process ... make this logical flow ... iterate until consistent and complete
+
+     variable names here are essentially edges
+
+     start() ---> x(i) for i n [0,N)
+
+     guess(x(i)) ---> xy[0](i)
+
+     iteration(xy[k](i)) --> xynew[k][i]
+
+     residual(xy[k][i], xynew[k][i]) --> errsq[i]
+
+     binarysum(errsq[i],k) --> errsqsum[k]
+
+     convergence_test[k](errsqsum[k],xynew[i]) --> sends xynew[k][i] to either result[i] or xy[k+1][i]
+     
+ ***/
 
 template <typename X, typename Y>
 std::ostream& operator<<(std::ostream& s, const std::pair<X,Y>& p) {
@@ -20,46 +67,183 @@ std::ostream& operator<<(std::ostream& s, const std::pair<X,Y>& p) {
     return s;
 }
 
+static inline bool is_power_of_two(size_t N) {
+    if (N==0) return true;
+    if (N==1) return false;
+    else return ((N-1)&N) == 0;
+}
+
+/// Makes an operation that reduces a vector of length N to a scalar that is sent to specified key
+template <typename sumresultkeyT, typename valueT>
+auto make_binary_reduce(Edge<size_t,valueT>& in, Edge<sumresultkeyT,valueT>& result, size_t N, const sumresultkeyT& result_key) {
+    using iNT = std::pair<size_t,size_t>;
+    using outT = std::tuple<Out<iNT,valueT>,Out<iNT,valueT>,Out<sumresultkeyT,valueT>>;
+    // Handles logic of propagating value up reduction tree and enventually to output
+    auto logic = [result_key](const iNT& iN, valueT sum, outT& out) {
+        const size_t i=iN.first, N=iN.second, Nisodd=(N&0x1);
+        iNT up {i>>1,(N+Nisodd)>>1};
+        if (N == 1)     ::send<2>(result_key, sum, out); // result
+        else if (i&0x1) ::send<1>(up, sum, out);         // right=odd
+        else            ::send<0>(up, sum, out);         // left=even
+        if (N>1 && i==(N-1) && Nisodd) ::send<1>(up, valueT(0.0), out); // Missing element
+    };
+        
+    // Takes vec[i] (i in [0,N]) as input and initiates reduction
+    auto startfn = [N,logic](const size_t& i, const valueT& value, outT& out) {logic(iNT(i,N), value, out);};
+    
+    auto reducefn = [logic](const iNT& iN, const valueT& left, const valueT& right, outT& out) {logic(iN, left+right, out);};
+    
+    Edge<iNT,valueT> left, right;
+    auto start = wrap<size_t>(startfn, edges(in), edges(left,right,result), "startsum", {"in"}, {"left","right","result"});
+    auto reduce= wrap<iNT>(reducefn, edges(left,right), edges(left,right,result), "reducesum", {"left","right"}, {"left","right","result"});
+    auto ins = std::make_tuple(start-> template in<0>());
+    auto outs = std::make_tuple(reduce-> template out<2>());
+    std::vector<std::unique_ptr<OpBase>> ops(2);
+    ops[0] = std::move(start); 
+    ops[1] = std::move(reduce);
+    return make_composite_op(std::move(ops), ins, outs, "reduce");    
+}
+
+using xT = double; // type of x
+using yT = double; // type of y
+using xyT = std::pair<xT,yT>; // holds x[i],y[i]
+using iT = size_t; // i is the index into the vector
+using kT = size_t; // k is the iteration index
+using kiT = std::pair<kT,iT>; // ki holds iteration k and vector index i
+using sumreskeyT = int; // type of key for sum destination
+
+namespace std {
+    template <> struct hash<kiT> {
+        size_t operator()(const kiT& s) const noexcept { return (s.first<<32) + s.second; }
+    };
+}
+
+
+/// Start computation and produce x
+auto make_start(Edge<iT,xT>& x, size_t N) {
+    auto f = [N](const iT& unused, std::tuple<Out<iT,xT>>& out) {
+        for (iT i=0; i<N; i++) ::send<0>(i, drand48(), out);
+    };
+    return wrap<iT>(f, edges(), edges(x), "start", {}, {"x"});
+}
+
+// Produce initial guess for y and forward xy
+auto make_guess(Edge<iT,xT>& x, Edge<kiT,xyT>& xy) {
+    auto f = [](const iT& i, const xT& x, std::tuple<Out<kiT,xyT>>& out) {
+        yT y = 6.0+(8.0*x-12.0)*x; // quadratic part of taylor expansion about 0.5 (assuming x in (0,1))
+        ::send<0>(kiT(0,i), xyT(x,y), out);
+    };
+    return wrap<iT>(f, edges(x), edges(xy), "start", {"x"}, {"xy"});
+}
+
+/// Update y and forward xy
+auto make_iteration(Edge<kiT,xyT>&& xy, Edge<kiT,xyT>& xynew) {
+    auto f = [](const kiT& ki, const xyT& xy, std::tuple<Out<kiT,xyT>>& out) {
+        xT x = xy.first;
+        yT y = xy.second;
+        yT ynew = y*(2.0-x*y);
+        ::send<0>(ki, xyT(x,ynew), out);
+    };
+    return wrap<kiT>(f, edges(xy), edges(xynew), "iteration", {"xy"}, {"xynew"});
+}
+
+/// Compute (yold[i]-ynew[i])**2
+auto make_residual(Edge<kiT,xyT>&& xy, Edge<kiT,xyT>& xynew, Edge<sumreskeyT,kT>& k, Edge<iT,yT>& errsq) {
+    auto f = [](const kiT& ki, const xyT& xy, const xyT& xynew, std::tuple<Out<sumreskeyT,kT>,Out<iT,yT>>& out) {
+        yT err = (xy.second - xynew.second);
+        const kT k = ki.first;
+        const iT i = ki.second;
+        if (i == 0) ::send<0>(sumreskeyT(0), k, out);
+        ::send<1>(i, err*err, out);
+    };
+    return wrap<kiT>(f, edges(xy,xynew), edges(k,errsq), "residual", {"xy","xynew"}, {"k","errsq"});
+}
+
+auto make_sum_result(Edge<sumreskeyT,kT>& k, Edge<sumreskeyT,yT>& sumresult, Edge<kT,yT>& errsqsum) {
+    auto f = [](const sumreskeyT& unused, const kT& k, const yT& sumresult, std::tuple<Out<kT,yT>>& out) {
+        ::send<0>(k, sumresult, out);
+    };
+    return wrap<sumreskeyT>(f, edges(k,sumresult), edges(errsqsum), "sumres", {"k","sum"}, {"sum"});
+}
+
+/// Use residual for this iteration (k) to produce boolean converged flag
+auto make_converged(Edge<kT,yT>& errsqsum, Edge<kT,bool>& converged) { 
+    auto f = [](const kT& k, const yT& errsqsum, std::tuple<Out<kT,bool>>& out) {
+        bool converged = (errsqsum < 1e-10);
+        ::ttg::print("iteration ", k, " residual ", std::sqrt(errsqsum), " converged ", converged?"yes":"no");
+        ::send<0>(k,converged, out);
+    };
+    return wrap<kT>(f, edges(errsqsum), edges(converged), "convtest", {"errsqsum"}, {"converged"});
+}
+
+/// Broadcast convergence flag for this iteration [k] to all vector
+/// elements ... broken out from make_convergence_test to make this
+/// more explicit and visible ... we only need N because we don't yet
+/// have the sparse/wildcard broadcast, so we are very inefficiently
+/// broadcasting to all i in [0,N) ... once we have the broadcast operation
+/// this step is eliminated.
+auto make_broadcast(Edge<kT,bool>& converged, Edge<kiT,bool>& bcast, size_t N) {
+    auto f = [N](const kT& k, const bool& converged, std::tuple<Out<kiT,bool>>& out) {
+        for (iT i=0; i<N; i++) ::send<0>(kiT(k,i),converged,out);
+    };
+    return wrap<kT>(f, edges(converged), edges(bcast), "bcast", {"converged"}, {"bcast"});
+}
+
+/// If converged produce result, otherwise continue to iterate
+auto make_loop(Edge<kiT,bool>& bcast, Edge<kiT,xyT>& xynew, Edge<kiT,xyT>& xy, Edge<iT,xyT>& result) {
+    auto f = [](const kiT& ki, const bool& converged, const xyT& xynew, std::tuple<Out<kiT,xyT>,Out<iT,xyT>>& out) {
+        if (converged) {
+            send<1>(ki.second, xynew, out); // result
+        }
+        else {
+            send<0>(ki, xynew, out); // iterate
+        }
+    };
+    return wrap<kiT>(f, edges(bcast, xynew), edges(xy, result), "loop", {"bcast", "xynew"}, {"xy", "result"});
+}
+
+/// Makes an operation that prints a stream
 template <typename keyT, typename valueT>
 auto make_printer(const Edge<keyT, valueT>& in, const char* str = "") {
-  auto func = [str](const keyT& key, const valueT& value, std::tuple<>& out) {
-    std::cout << str << " (" << key << "," << value << ")" << std::endl;
-  };
-  return wrap(func, edges(in), edges(), "printer", {"input"});
-}
-
-void iteration(const int& iter, const std::pair<double,double>& xy, std::tuple<Out<int,std::pair<double,double>>, Out<int,std::pair<double,double>>>& out) {
-    double x, y; std::tie(x,y) = xy;
-    double ynew = y*(2.0-x*y);
-    std::cout << "iteration " << iter << " y " << y << " ynew " << ynew << std::endl;
-    if (std::abs(y-ynew) < 1e-10*std::abs(y)) {
-        ::send<1>(iter,std::make_pair(x,ynew),out);
-    }
-    else {
-        ::send<0>(iter+1,std::make_pair(x,ynew),out);
-    }
-}    
-
-auto make_start(Edge<int,std::pair<double,double>>& out, double x, double yguess) {
-    using outT = std::tuple<Out<int,std::pair<double,double>>>;
-    auto f = [x,yguess](const int& key, const std::tuple<>& junk, outT& out) {
-        ::send<0>(0,std::make_pair(x,yguess),out);
+    auto func = [str](const keyT& key, const valueT& value, std::tuple<>& out) {
+        ::ttg::print(str,":",key,":",value);
     };
-    // using funcT = decltype(f);
-    // using wrapopT = WrapOp<funcT, int, outT>;
-    // return std::unique_ptr<wrapopT>(new wrapopT(f, edges(), edges(out), "start", {}, {"guess"}));
-    return wrapt<int>(f, edges(), edges(out), "start", {}, {"guess"});
+    return wrap(func, edges(in), edges(), "printer", {"input"});
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
     ttg_initialize(argc, argv, 2);
+    std::cout << "Hello from reciprocal\n";
 
-    {
-        Edge<int,std::pair<double,double>> a("iteration"), b("result");
-        auto start = make_start(a,2.0,0.9);
-        auto iter = wrap(iteration, edges(a), edges(a,b), "iteration", {"xy"},{"xy","result"});
-        auto prt = make_printer(b);
+    // Give random number generator process-dependent state.  Also
+    // drand48 seems to need warming up.
+    srand48(ttg_default_execution_context().rank());
+    for (size_t i=0; i<100; i++) drand48();
 
+    try {
+        Edge<iT,xT> x;
+        Edge<iT,xyT> result;
+        Edge<kT,yT> errsq, errsqsum;
+        Edge<kT,bool> converged;
+        Edge<kiT,xyT> xy, xynew, xyguess;
+        Edge<kiT,bool> bcast;
+        Edge<sumreskeyT,yT> sumresult;
+        Edge<sumreskeyT,kT> k;
+        
+        const size_t N = 99;
+        
+        auto start = make_start(x, N);
+        auto guess = make_guess(x, xyguess);
+        auto iteration = make_iteration(fuse(xy,xyguess), xynew);
+        auto residual = make_residual(fuse(xy,xyguess), xynew, k, errsq);
+        auto binsum = make_binary_reduce(errsq, sumresult, N, sumreskeyT(0));
+        auto sumres = make_sum_result(k, sumresult, errsqsum);
+        auto conv = make_converged(errsqsum, converged);
+        auto bcastop = make_broadcast(converged, bcast, N);
+        auto loop = make_loop(bcast, xynew, xy, result);
+        auto printer = make_printer(result, "result: ");
+        
         auto connected = make_graph_executable(start.get());
         assert(connected);
         if (ttg_default_execution_context().rank() == 0) {
@@ -75,8 +259,13 @@ int main(int argc, char** argv) {
         ttg_execute(ttg_default_execution_context());
         ttg_fence(ttg_default_execution_context());
     }
+    catch (std::string e) {
+        std::cout << "std::string Exception: " << e << std::endl;
+    }
+    catch (const char* e) {
+        std::cout << "char* Exception: " << e << std::endl;
+    }
     
-
     get_default_world().gop.fence();
     ttg_finalize();
     return 0;
