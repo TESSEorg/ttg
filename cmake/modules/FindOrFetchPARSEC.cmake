d0b45899a6e3ea39f49dadd7d545de853f9a84e1
Eduard Valeyev <eduard@valeyev.net>
2020-03-05 08:21:01 -0500

    embedded PaRSEC as subproject ... WIP: there is still a "docs" CMake target name conflict with MADNESS

---
+++ cmake/modules/FindOrFetchPARSEC.cmake
@@ -0,0 +1,21 @@
+find_package(PARSEC CONFIG QUIET COMPONENTS parsec HINTS ${PARSEC_ROOT_DIR})
+
+if (NOT TARGET PaRSEC::parsec)
+
+  # configure PaRSEC
+  set(SUPPORT_FORTRAN OFF CACHE BOOL "Disable Fortran support in PaRSEC")
+  set(CMAKE_CROSSCOMPILING OFF)
+  set(CMAKE_SYSTEM_PROCESSOR ${CMAKE_HOST_SYSTEM_PROCESSOR})
+  set(PARSEC_WITH_DEVEL_HEADERS ON CACHE BOOL "Install PaRSEC headers")
+
+  include(DownloadProject)
+  download_project(PROJ                PARSEC
+    GIT_REPOSITORY      https://bitbucket.org/eduard_valeyev/parsec.git
+    GIT_TAG             ${TTG_TRACKED_PARSEC_TAG}
+    PREFIX              ${PROJECT_BINARY_DIR}/external
+    UPDATE_DISCONNECTED 1
+    )
+
+  add_subdirectory(${PARSEC_SOURCE_DIR} ${PARSEC_BINARY_DIR})
+
+endif(NOT TARGET PaRSEC::parsec)
