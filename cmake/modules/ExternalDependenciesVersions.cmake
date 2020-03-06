0c9eb079b35cbe11b6fb371ea27a0aee3f2e7281
Eduard Valeyev <eduard@valeyev.net>
2020-03-05 19:01:18 -0500

    [cmake] bumped parsec tag

--- cmake/modules/ExternalDependenciesVersions.cmake
+++ cmake/modules/ExternalDependenciesVersions.cmake
@@ -4,4 +4,4 @@
 # need Boost.CallableTraits (header only, part of Boost 1.66 released in Dec 2017) for wrap.h to work
 set(TTG_TRACKED_BOOST_VERSION 1.66)
 set(TTG_TRACKED_MADNESS_TAG 84feeba24d893f5250f80f6a0aa267a5f73830e1)
-set(TTG_TRACKED_PARSEC_TAG 65dac9c22c9913dc4dc085ff2f52e696051298cd)
+set(TTG_TRACKED_PARSEC_TAG fd09f737cd5fd03e66039ce213731bfa177dcb04)
