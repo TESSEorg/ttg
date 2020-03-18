ee39bb90ca30aa6db347e83f74c06a03476de5b8
Eduard Valeyev <eduard@valeyev.net>
2020-03-18 11:36:32 -0400

    bumped MAD tag

--- cmake/modules/ExternalDependenciesVersions.cmake
+++ cmake/modules/ExternalDependenciesVersions.cmake
@@ -3,5 +3,5 @@
 
 # need Boost.CallableTraits (header only, part of Boost 1.66 released in Dec 2017) for wrap.h to work
 set(TTG_TRACKED_BOOST_VERSION 1.66)
-set(TTG_TRACKED_MADNESS_TAG 84feeba24d893f5250f80f6a0aa267a5f73830e1)
+set(TTG_TRACKED_MADNESS_TAG 977bd48793fd609641983a8ba4fa18c8b9882e4f)
 set(TTG_TRACKED_PARSEC_TAG 1ca1d5f53bc7417395a12b8e6140a072cc0098ac)
