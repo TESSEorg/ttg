7f54cf68eb711400d1f366453303feb7d1501f22
Eduard Valeyev <eduard@valeyev.net>
2020-04-03 08:13:15 -0400

    switched to FetchContent v1

--- cmake/modules/FindOrFetchBoost.cmake
+++ cmake/modules/FindOrFetchBoost.cmake
@@ -2,18 +2,19 @@ find_package(Boost ${TTG_TRACKED_BOOST_VERSION} QUIET)
 
 if (NOT TARGET Boost::boost)
 
-  include(DownloadProject)
-  download_project(PROJ                BOOST
-    GIT_REPOSITORY      https://github.com/Orphis/boost-cmake
-    PREFIX              "${PROJECT_BINARY_DIR}/external"
-    UPDATE_DISCONNECTED 1
-    )
-
-  add_subdirectory("${BOOST_SOURCE_DIR}" "${BOOST_BINARY_DIR}")
-
-  if (NOT TARGET Boost::boost)
-    message(FATAL_ERROR "Downloaded and configured boost, but Boost::boost is still not found. Please create an issue at ${PROJECT_HOMEPAGE_URL}")
-  endif(NOT TARGET Boost::boost)
+  FetchContent_Declare(
+          BOOST
+          GIT_REPOSITORY      https://github.com/Orphis/boost-cmake
+  )
+  FetchContent_MakeAvailable(BOOST)
+  FetchContent_GetProperties(BOOST
+          SOURCE_DIR BOOST_SOURCE_DIR
+          BINARY_DIR BOOST_BINARY_DIR
+          )
 
 endif(NOT TARGET Boost::boost)
 
+# postcond check
+if (NOT TARGET Boost::boost)
+  message(FATAL_ERROR "FindOrFetchBOOST could not make Boost::boost target available")
+endif(NOT TARGET Boost::boost)
--- cmake/modules/FindOrFetchMADNESS.cmake
+++ cmake/modules/FindOrFetchMADNESS.cmake
@@ -2,14 +2,20 @@ find_package(MADNESS 0.10.1 CONFIG QUIET COMPONENTS world HINTS "${MADNESS_ROOT_
 
 if (NOT TARGET MADworld)
 
-  include(DownloadProject)
-  download_project(PROJ                MADNESS
-    GIT_REPOSITORY      https://github.com/m-a-d-n-e-s-s/madness.git
-    GIT_TAG             ${TTG_TRACKED_MADNESS_TAG}
-    PREFIX              "${PROJECT_BINARY_DIR}/external"
-    UPDATE_DISCONNECTED 1
-    )
+  FetchContent_Declare(
+          MADNESS
+          GIT_REPOSITORY      https://github.com/m-a-d-n-e-s-s/madness.git
+          GIT_TAG             ${TTG_TRACKED_MADNESS_TAG}
+  )
+  FetchContent_MakeAvailable(MADNESS)
+  FetchContent_GetProperties(MADNESS
+          SOURCE_DIR MADNESS_SOURCE_DIR
+          BINARY_DIR MADNESS_BINARY_DIR
+          )
 
-  add_subdirectory("${MADNESS_SOURCE_DIR}" "${MADNESS_BINARY_DIR}")
+endif(NOT TARGET MADworld)
 
+# postcond check
+if (NOT TARGET MADworld)
+  message(FATAL_ERROR "FindOrFetchMADNESS could not make MADworld target available")
 endif(NOT TARGET MADworld)
