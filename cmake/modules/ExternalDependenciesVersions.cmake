3f188d770777b478839309864e03ee25985ed73a
Eduard Valeyev <eduard@valeyev.net>
2020-04-03 09:36:42 -0400

    bump madness to the master rev containing as-subproject branch

--- cmake/modules/ExternalDependenciesVersions.cmake
+++ cmake/modules/ExternalDependenciesVersions.cmake
@@ -3,5 +3,5 @@
 
 # need Boost.CallableTraits (header only, part of Boost 1.66 released in Dec 2017) for wrap.h to work
 set(TTG_TRACKED_BOOST_VERSION 1.66)
-set(TTG_TRACKED_MADNESS_TAG 1375932d2214a2895029af27ce77f9136566bfe9)
+set(TTG_TRACKED_MADNESS_TAG c1ce2f94b81c28d844d6aa0b226bd1e2cdfa9714)
 set(TTG_TRACKED_PARSEC_TAG 1ca1d5f53bc7417395a12b8e6140a072cc0098ac)
