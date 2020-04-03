71f1337e6de874cd01e1e9493302035fac206afb
Thomas Herault <herault@icl.utk.edu>
2020-04-03 10:03:56 -0400

    Update the PaRSEC version to the one that temporary fixes the funneled_mpi initialization

--- cmake/modules/ExternalDependenciesVersions.cmake
+++ cmake/modules/ExternalDependenciesVersions.cmake
@@ -4,4 +4,4 @@
 # need Boost.CallableTraits (header only, part of Boost 1.66 released in Dec 2017) for wrap.h to work
 set(TTG_TRACKED_BOOST_VERSION 1.66)
 set(TTG_TRACKED_MADNESS_TAG 977bd48793fd609641983a8ba4fa18c8b9882e4f)
-set(TTG_TRACKED_PARSEC_TAG 1ca1d5f53bc7417395a12b8e6140a072cc0098ac)
+set(TTG_TRACKED_PARSEC_TAG a7eaf9c62ce99efc61503aeccd39cd99d582cae4)
