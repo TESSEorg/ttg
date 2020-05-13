865fb275e32404bb8ac5366ab5ec2fa5e7433f7f
Thomas Herault <herault@icl.utk.edu>
2020-05-12 21:34:30 -0400

    Refresh to the last update of parsec -- single node works, there is a bug in distributed (keymap(0) returns 1 in test-parsec, there is a bad set_arg in t9-parsec)

--- cmake/modules/ExternalDependenciesVersions.cmake
+++ cmake/modules/ExternalDependenciesVersions.cmake
@@ -4,4 +4,4 @@
 # need Boost.CallableTraits (header only, part of Boost 1.66 released in Dec 2017) for wrap.h to work
 set(TTG_TRACKED_BOOST_VERSION 1.66)
 set(TTG_TRACKED_MADNESS_TAG c1ce2f94b81c28d844d6aa0b226bd1e2cdfa9714)
-set(TTG_TRACKED_PARSEC_TAG f9f9ee0d6b1a5f8c96c0410c01af18950ca56631)
+set(TTG_TRACKED_PARSEC_TAG 4428de2f7ca9d63fa332e139c86d66067b498693)
