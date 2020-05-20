950ed48f68ef79182a9c32a7832125f3fb83a255
Eduard Valeyev <eduard@valeyev.net>
2020-05-20 11:20:08 -0400

    peg MADNESS to the branch to revive MAD's PaRSEC backend  .. to try (and fail) set ENABLE_PARSEC=ON

--- cmake/modules/ExternalDependenciesVersions.cmake
+++ cmake/modules/ExternalDependenciesVersions.cmake
@@ -3,5 +3,5 @@
 
 # need Boost.CallableTraits (header only, part of Boost 1.66 released in Dec 2017) for wrap.h to work
 set(TTG_TRACKED_BOOST_VERSION 1.66)
-set(TTG_TRACKED_MADNESS_TAG fb8063177b2d1e79ea20d3d4880759ea3953540e)
+set(TTG_TRACKED_MADNESS_TAG a7da8be01c86b805e60ddf8d54bd8bd71852cc2b)
 set(TTG_TRACKED_PARSEC_TAG 4428de2f7ca9d63fa332e139c86d66067b498693)
