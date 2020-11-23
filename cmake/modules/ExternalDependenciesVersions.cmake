438d5500f14ecf42796799a9e042443464d5a2ae
Thomas Herault <herault@icl.utk.edu>
2020-11-23 09:42:10 -0500

    Update to latest commit in parsec termdet branch.

--- cmake/modules/ExternalDependenciesVersions.cmake
+++ cmake/modules/ExternalDependenciesVersions.cmake
@@ -4,4 +4,4 @@
 # need Boost.CallableTraits (header only, part of Boost 1.66 released in Dec 2017) for wrap.h to work
 set(TTG_TRACKED_BOOST_VERSION 1.66)
 set(TTG_TRACKED_MADNESS_TAG 8a3013d635d757a4899ed6c52a86c1ccfe530e33)
-set(TTG_TRACKED_PARSEC_TAG 0b9784ba02527a49aa99b7c70f7c55ce2f51bc3b)
+set(TTG_TRACKED_PARSEC_TAG 70b92123e73328f8bcd706aa33b4e13fcf867f49)
