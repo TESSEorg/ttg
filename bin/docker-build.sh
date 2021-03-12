8695e4d60b336e6aadce85ef0ad0f18ba098a3be
Eduard Valeyev <eduard@valeyev.net>
2021-03-12 16:33:37 -0500

    bump image tag to the latest + support arm64
    - using phusion/baseimage:master-<arch>
    - can build for amd64 or arm64

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # this script builds a TTG docker image
+# usage: docker-build.sh <arch>
+# where <arch> is one of the following: amd64 arm64
 
 # to run bash in the image: docker run --privileged -i -t ttg-dev:latest /sbin/my_init -- bash -l
 # see docker.md for further instructions
@@ -9,9 +11,22 @@
 #   - build dir: /home/tesse/ttg-build
 #   - build dir: /home/tesse/ttg-install
 
-export CMAKE_VERSION=3.17.0
+if [ "$#" -ne 1 ] || ([ "$1" != "amd64" ] && [ "$1" != "arm64" ]); then
+  echo "Usage: $0 <arch> , with <arch> either amd64 or arm64" >&2
+  exit 1
+fi
+
+export CMAKE_VERSION=3.19.5
 export DIRNAME=`dirname $0`
 export ABSDIRNAME=`pwd $DIRNAME`
+if [ "$1" = "amd64" ]; then
+  export ARCH=amd64
+  export ARCH_CMAKE=x86_64
+fi
+if [ "$1" = "arm64" ]; then
+  export ARCH=arm64
+  export ARCH_CMAKE=aarch64
+fi
 
 ##############################################################
 # make a script to disable ASLR to make MADWorld happy
@@ -30,7 +45,7 @@ cat > Dockerfile << END
 # to 'latest'! See
 # https://github.com/phusion/baseimage-docker/blob/master/Changelog.md
 # for a list of version numbers.
-FROM phusion/baseimage:0.11
+FROM phusion/baseimage:master-${ARCH}
 
 # Use baseimage-docker's init system.
 CMD ["/sbin/my_init"]
@@ -42,7 +57,7 @@ RUN apt-get update && apt-get upgrade -y -o Dpkg::Options::="--force-confold"
 # 1. basic prereqs
 RUN apt-get update && apt-get install -y cmake ninja-build libblas-dev liblapack-dev liblapacke-dev bison flex mpich libboost-dev libeigen3-dev git wget libtbb-dev clang-8 libc++-8-dev libc++abi-8-dev && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 # 2. recent cmake
-RUN CMAKE_URL="https://cmake.org/files/v${CMAKE_VERSION%.[0-9]}/cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz" && wget --no-check-certificate -O - \$CMAKE_URL | tar --strip-components=1 -xz -C /usr/local
+RUN CMAKE_URL="https://cmake.org/files/v${CMAKE_VERSION%.[0-9]}/cmake-${CMAKE_VERSION}-Linux-${ARCH_CMAKE}.tar.gz" && wget --no-check-certificate -O - \$CMAKE_URL | tar --strip-components=1 -xz -C /usr/local
 ENV CMAKE=/usr/local/bin/cmake
 # 3. clone and build TTG
 RUN mkdir -p /home/tesse && cd /home/tesse && git clone https://github.com/TESSEorg/ttg.git && mkdir ttg-build && cd ttg-build && \$CMAKE ../ttg -GNinja -DCMAKE_CXX_COMPILER=clang++-8 -DCMAKE_C_COMPILER=clang-8 -DCMAKE_INSTALL_PREFIX=/home/tesse/ttg-install -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF && \$CMAKE --build .
