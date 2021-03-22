625fcca728d02631de6ec63bd39559ed5b4fda60
Joseph Schuchart <schuchart@hlrs.de>
2021-03-22 15:35:29 +0100

    Add documentation for SplitMetadataDescriptor
    Signed-off-by: Joseph Schuchart <schuchart@icl.utk.edu>

--- ttg/ttg/serialization/splitmd_data_descriptor.h
+++ ttg/ttg/serialization/splitmd_data_descriptor.h
@@ -5,11 +5,41 @@
 
 namespace ttg {
 
+  /**
+   * Used to describe transfer payload in types using the \sa SplitMetadataDescriptor.
+   * @member data Pointer to the data to be read from / written to.
+   * @member num_bytes The number of bytes to read from / write to the memory location
+   *                   \sa data.
+   */
   struct iovec {
     size_t num_bytes;
     void*     data;
-  };  
+  };
 
+  /**
+   * SplitMetadataDescriptor is a serialization descriptor provided by the user
+   * for a user-specified type. I should contain the following public member
+   * functions:
+   *
+   * <metadata_type> get_metadata(const T& t);
+   *
+   * Returns the metadata that describes the object, e.g., shape information.
+   * This data will be passed to
+   *
+   * auto create_from_metadata(const <metadata_type>& meta);
+   *
+   * which is expected to return a new instance of T, initialized with the
+   * previously provided metadata. This instance will be deserialization target.
+   *
+   * Both the serialization source and the deserialization target objects will
+   * be passed to
+   *
+   * auto get_data(T& t);
+   *
+   * which is expected to return a collection of \sa ttg::iovec instances
+   * describing the payload data to be transferred from the source to the
+   * target object.
+   */
   template<typename T>
   struct SplitMetadataDescriptor;
 
@@ -22,7 +52,6 @@ namespace ttg {
   struct has_split_metadata<T, ttg::meta::void_t<decltype(std::declval<SplitMetadataDescriptor<T>>().get_metadata(std::declval<T>()))>>
   : std::true_type
   { };
-
 
 }  // namespace ttg
 
