84b8cb518050de65a83ed874b885e30ebfd1657e
Eduard Valeyev <eduard@valeyev.net>
2021-05-14 13:29:21 -0400

    boost serialization: std::allocator support
    serializing std::vector and std::list requires serializing the allocator

--- ttg/ttg/serialization/std/list.h
+++ ttg/ttg/serialization/std/list.h
@@ -5,6 +5,7 @@
 #ifndef TTG_SERIALIZATION_STD_LIST_H
 #define TTG_SERIALIZATION_STD_LIST_H
 
+#include "ttg/serialization/std/allocator.h"
 #include "ttg/serialization/traits.h"
 
 #ifdef TTG_SERIALIZATION_SUPPORTS_BOOST
@@ -13,10 +14,10 @@
 namespace ttg::detail {
   template <typename Archive, typename T, typename A>
   inline static constexpr bool is_stlcontainer_boost_serializable_v<Archive, std::list<T, A>> =
-      is_boost_serializable_v<Archive, T>;
+      is_boost_serializable_v<Archive, T>&& is_boost_serializable_v<Archive, A>;
   template <typename Archive, typename T, std::size_t N>
   inline static constexpr bool is_stlcontainer_boost_serializable_v<Archive, const std::list<T, A>> =
-      is_boost_serializable_v<Archive, const T>;
+      is_boost_serializable_v<Archive, const T>&& is_boost_serializable_v<Archive, const A>;
 }  // namespace ttg::detail
 
 #endif  // TTG_SERIALIZATION_SUPPORTS_BOOST
