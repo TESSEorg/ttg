9847ab40921b5ac84ae679d7477f49c8e500c5f1
Eduard Valeyev <eduard@valeyev.net>
2021-05-14 13:27:19 -0400

    serialization source tree reorg
    all backend-specific headers go to backends/

--- ttg/ttg/serialization/backends.h
+++ ttg/ttg/serialization/backends.h
@@ -5,8 +5,8 @@
 #ifndef TTG_SERIALIZATION_BACKENDS_H
 #define TTG_SERIALIZATION_BACKENDS_H
 
-#include "ttg/serialization/boost.h"
-#include "ttg/serialization/cereal.h"
-#include "ttg/serialization/madness.h"
+#include "ttg/serialization/backends/boost.h"
+#include "ttg/serialization/backends/cereal.h"
+#include "ttg/serialization/backends/madness.h"
 
 #endif  // TTG_SERIALIZATION_ALL_H
