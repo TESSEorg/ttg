6208abf5391342c3f77067b69ed894688fcf5f7c
Eduard Valeyev <eduard@valeyev.net>
2021-05-17 17:25:19 -0400

    introduced iovec streambufs

--- ttg/ttg/serialization/stream.h
+++ ttg/ttg/serialization/stream.h
@@ -26,6 +26,45 @@ namespace ttg::detail {
    private:
     size_t size_ = 0;
   };
+
+  /// streambuf that records vector of address-size pairs
+  class iovec_ostreambuf : public std::streambuf {
+   public:
+    using std::streambuf::streambuf;
+
+    const auto& iovec() const { return iovec_; };
+
+   protected:
+    std::streamsize xsputn(const char_type* s, std::streamsize n) override {
+      iovec_.emplace_back(s, n);
+      return n;
+    }
+
+   private:
+    std::vector<std::pair<const char*, std::size_t>> iovec_ = {};
+  };
+
+  /// streambuf that reads vector of address-size pairs
+  class iovec_istreambuf : public std::streambuf {
+   public:
+    using std::streambuf::streambuf;
+
+    iovec_istreambuf(const std::vector<std::pair<const char*, std::size_t>>& iovec) : iovec_(iovec) {}
+
+   protected:
+    std::streamsize xsgetn(char_type* s, std::streamsize max_n) override {
+      const auto n = iovec_[current_item_].second;
+      if (n > max_n)
+        throw std::out_of_range("iovec_istreambuf::xsgetn(dest, max_n): actual size of data exceeds max_n");
+      std::copy(iovec_[current_item_].first, iovec_[current_item_].first + n, s);
+      return n;
+    }
+
+   private:
+    std::size_t current_item_ = 0;
+    const std::vector<std::pair<const char*, std::size_t>>& iovec_;
+  };
+
 }  // namespace ttg::detail
 
 #endif  // TTG_SERIALIZATION_STREAM_H
