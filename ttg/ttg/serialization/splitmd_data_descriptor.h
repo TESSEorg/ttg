d67fe78bdf9b0fea648bf3aea61a9e811f2f4427
Eduard Valeyev <eduard@valeyev.net>
2021-06-18 10:32:41 -0400

    dox cleanup

--- ttg/ttg/serialization/splitmd_data_descriptor.h
+++ ttg/ttg/serialization/splitmd_data_descriptor.h
@@ -14,45 +14,44 @@ namespace ttg {
    */
   struct iovec {
     size_t num_bytes;
-    void*     data;
+    void* data;
   };
 
   /**
    * SplitMetadataDescriptor is a serialization descriptor provided by the user
-   * for a user-specified type. I should contain the following public member
+   * for a user-specified type. It should contain the following public member
    * functions:
-   *
-   * <metadata_type> get_metadata(const T& t);
-   *
-   * Returns the metadata that describes the object, e.g., shape information.
+   * @code
+   *   <metadata_type> get_metadata(const T& t);
+   * @endcode
+   * which returns the metadata that describes the object, e.g., shape information.
    * This data will be passed to
-   *
-   * auto create_from_metadata(const <metadata_type>& meta);
-   *
-   * which is expected to return a new instance of T, initialized with the
+   * @code
+   *   auto create_from_metadata(const <metadata_type>& meta);
+   * @endcode
+   * which returns a new instance of T, initialized with the
    * previously provided metadata. This instance will be deserialization target.
    *
    * Both the serialization source and the deserialization target objects will
    * be passed to
-   *
-   * auto get_data(T& t);
-   *
-   * which is expected to return a collection of \sa ttg::iovec instances
+   * @code
+   *   auto get_data(T& t);
+   * @endcode
+   * which returns a collection of \sa ttg::iovec instances
    * describing the payload data to be transferred from the source to the
    * target object.
    */
-  template<typename T>
+  template <typename T>
   struct SplitMetadataDescriptor;
 
   /* Trait signalling whether metadata and data payload can be transfered separately */
-  template<typename T, typename Enabler = void>
-  struct has_split_metadata : std::false_type
-  { };
+  template <typename T, typename Enabler = void>
+  struct has_split_metadata : std::false_type {};
 
-  template<typename T>
-  struct has_split_metadata<T, ttg::meta::void_t<decltype(std::declval<SplitMetadataDescriptor<T>>().get_metadata(std::declval<T>()))>>
-  : std::true_type
-  { };
+  template <typename T>
+  struct has_split_metadata<
+      T, ttg::meta::void_t<decltype(std::declval<SplitMetadataDescriptor<T>>().get_metadata(std::declval<T>()))>>
+      : std::true_type {};
 
 }  // namespace ttg
 
