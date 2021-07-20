697e1238ceff7d106b0ffe45950007f0537b2bfe
Poornima Nookala <nookala.poornima@gmail.com>
2021-07-20 17:43:49 -0500

    First working version of mrattg using streaming terminals

--- examples/madness/mrattg_streaming.cc
+++ examples/madness/mrattg_streaming.cc
@@ -31,8 +31,6 @@ using namespace ttg;
 #include "../mrafunctionnode.h"
 #include "../mrafunctionfunctor.h"
 
-//#include "/usr/include/mkl/mkl.h" // assume for now but need to wrap
-#include "mkl.h"
 
 using namespace mra;
 
@@ -448,7 +446,7 @@ public:
         const T L = Domain<NDIM>::get_max_width();
         const T a = expnt*L*L;
         double n = std::log(a/(4*K*K*(N*log10+std::log(fac))))/(2*log2);
-        //std::cout << expnt << " " << a << " " << n << std::endl;
+        std::cout << expnt << " " << a << " " << n << std::endl;
         initlev = Level(n<2 ? 2.0 : std::ceil(n));
     }
 
@@ -698,7 +696,7 @@ void test2(size_t nfunc, T thresh = 1e-6) {
 
 int main(int argc, char** argv) {
     ttg_initialize(argc, argv, 2);
-    //std::cout << "Hello from madttg\n";
+    std::cout << "Hello from madttg\n";
 
     //vmlSetMode(VML_HA | VML_FTZDAZ_OFF | VML_ERRMODE_DEFAULT); // default
     //vmlSetMode(VML_EP | VML_FTZDAZ_OFF | VML_ERRMODE_DEFAULT); // err is 10x default
