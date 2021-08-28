9013686bcdf42174ba618ebaffbff83e649b5395
Eduard Valeyev <eduard@valeyev.net>
2021-08-28 10:21:50 -0400

    added test of TTG from install tree

--- cmake/modules/FindOrFetchBTAS.cmake
+++ cmake/modules/FindOrFetchBTAS.cmake
@@ -1,4 +1,9 @@
-find_package(BTAS 1.0.0 QUIET)
+if (NOT TARGET BTAS::BTAS)
+  find_package(BTAS 1.0.0 QUIET CONFIG)
+  if (TARGET BTAS::BTAS)
+    message(STATUS "Found BTAS: BTAS_CONFIG=${BTAS_CONFIG}")
+  endif (TARGET BTAS::BTAS)
+endif (NOT TARGET BTAS::BTAS)
 
 if (NOT TARGET BTAS::BTAS)
 
@@ -14,12 +19,14 @@ if (NOT TARGET BTAS::BTAS)
       SOURCE_DIR BTAS_SOURCE_DIR
       BINARY_DIR BTAS_BINARY_DIR
       )
-  set_property(DIRECTORY ${BTAS_SOURCE_DIR} PROPERTY EXCLUDE_FROM_ALL TRUE)
 
   # use subproject targets as if they were in exported namespace ...
   if (TARGET BTAS AND NOT TARGET BTAS::BTAS)
     add_library(BTAS::BTAS ALIAS BTAS)
   endif(TARGET BTAS AND NOT TARGET BTAS::BTAS)
+
+  # set BTAS_CONFIG to the install location so that we know where to find it
+  set(BTAS_CONFIG ${CMAKE_INSTALL_PREFIX}/${BTAS_INSTALL_CMAKEDIR}/btas-config.cmake)
 
 endif(NOT TARGET BTAS::BTAS)
 
