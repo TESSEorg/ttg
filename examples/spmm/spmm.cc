4224de0314529ac77a5c6a1dce1bff820ee7fcb1
Joseph Schuchart <schuchart@icl.utk.edu>
2021-10-06 11:11:03 -0400

    Make inputs on Ops const and reuse the Control object
    Both changes avoid excessive copy creation and thus memory allocation.
    
    Signed-off-by: Joseph Schuchart <schuchart@icl.utk.edu>

--- examples/spmm/spmm.cc
+++ examples/spmm/spmm.cc
@@ -171,9 +171,9 @@ inline int tile2rank(int i, int j, int P, int Q) {
 
 // flow (move?) data into an existing SpMatrix on rank 0
 template <typename Blk = blk_t>
-class Write_SpMatrix : public Op<Key<2>, std::tuple<>, Write_SpMatrix<Blk>, Blk> {
+class Write_SpMatrix : public Op<Key<2>, std::tuple<>, Write_SpMatrix<Blk>, const Blk> {
  public:
-  using baseT = Op<Key<2>, std::tuple<>, Write_SpMatrix<Blk>, Blk>;
+  using baseT = Op<Key<2>, std::tuple<>, Write_SpMatrix<Blk>, const Blk>;
 
   template <typename Keymap>
   Write_SpMatrix(SpMatrix<Blk> &matrix, Edge<Key<2>, Blk> &in, Keymap &&keymap)
@@ -876,10 +876,10 @@ class SpMM {
 
   /// Central coordinator: ensures that all progress according to the plan
   class Coordinator : public Op<Key<2>, std::tuple<Out<Key<4>, Control>, Out<Key<4>, Control>, Out<Key<2>, Control>>,
-                                Coordinator, Control> {
+                                Coordinator, const Control> {
    public:
     using baseT =
-        Op<Key<2>, std::tuple<Out<Key<4>, Control>, Out<Key<4>, Control>, Out<Key<2>, Control>>, Coordinator, Control>;
+        Op<Key<2>, std::tuple<Out<Key<4>, Control>, Out<Key<4>, Control>, Out<Key<2>, Control>>, Coordinator, const Control>;
 
     Coordinator(Edge<Key<2>, Control> progress_ctl, Edge<Key<4>, Control> &a_ctl, Edge<Key<4>, Control> &b_ctl,
                 Edge<Key<2>, Control> &c2c_ctl, std::shared_ptr<const Plan> plan, const Keymap &keymap)
@@ -912,7 +912,7 @@ class SpMM {
             ttg::print("Coordinator(", r, ",", s, "): there are 0 local GEMMS in step", s,
                        "; triggering next coordinator step", s + plan_->lookahead_);
           baseT::template set_argstream_size<0>(Key<2>({r, s + plan_->lookahead_}), 1);
-          ::send<2>(Key<2>({r, s + plan_->lookahead_}), Control{}, out);
+          ::send<2>(Key<2>({r, s + plan_->lookahead_}), std::get<0>(input), out);
         }
       }
 
@@ -932,14 +932,14 @@ class SpMM {
           if (tracing())
             ttg::print("On rank", r, "Coordinator(", r, ", ", s, "): Sending control to LBCastA(", r, ",", gi, ",", gk,
                        ",", s, ")");
-          ::send<0>(Key<4>({r, gi, gk, s}), Control{}, out);
+          ::send<0>(Key<4>({r, gi, gk, s}), std::get<0>(input), out);
           seen_a.insert(std::make_tuple(gi, gk));
         }
         if (seen_b.find(std::make_tuple(gk, gj)) == seen_b.end()) {
           if (tracing())
             ttg::print("On rank", r, "Coordinator(", r, ", ", s, "): Sending control to LBCastB(", r, ",", gk, ",", gj,
                        ",", s, ")");
-          ::send<1>(Key<4>({r, gk, gj, s}), Control{}, out);
+          ::send<1>(Key<4>({r, gk, gj, s}), std::get<0>(input), out);
           seen_b.insert(std::make_tuple(gk, gj));
         }
       }
@@ -951,9 +951,9 @@ class SpMM {
   };
 
   // flow data from an existing SpMatrix
-  class Read_SpMatrix : public Op<Key<2>, std::tuple<Out<Key<2>, Blk>>, Read_SpMatrix, Control> {
+  class Read_SpMatrix : public Op<Key<2>, std::tuple<Out<Key<2>, Blk>>, Read_SpMatrix, const Control> {
    public:
-    using baseT = Op<Key<2>, std::tuple<Out<Key<2>, Blk>>, Read_SpMatrix, Control>;
+    using baseT = Op<Key<2>, std::tuple<Out<Key<2>, Blk>>, Read_SpMatrix, const Control>;
 
     Read_SpMatrix(const char *label, const SpMatrix<Blk> &matrix, Edge<Key<2>, Control> &progress_ctl,
                   Edge<Key<2>, Blk> &out, std::shared_ptr<const Plan> plan, const Keymap &keymap)
@@ -1058,9 +1058,9 @@ class SpMM {
   };
 
   /// broadcast A[i][k] to all procs where B[j][k]
-  class BcastA : public Op<Key<2>, std::tuple<Out<Key<3>, Blk>>, BcastA, Blk> {
+  class BcastA : public Op<Key<2>, std::tuple<Out<Key<3>, Blk>>, BcastA, const Blk> {
    public:
-    using baseT = Op<Key<2>, std::tuple<Out<Key<3>, Blk>>, BcastA, Blk>;
+    using baseT = Op<Key<2>, std::tuple<Out<Key<3>, Blk>>, BcastA, const Blk>;
 
     BcastA(Edge<Key<2>, Blk> &a_mn, Edge<Key<3>, Blk> &a_rik, std::shared_ptr<const Plan> plan, const Keymap &keymap)
         : baseT(edges(a_mn), edges(a_rik), "SpMM::BcastA", {"a_mn"}, {"a_rik"}, keymap), plan_(plan), keymap_(keymap) {}
@@ -1136,9 +1136,9 @@ class SpMM {
   };  // class LStoreA
 
   /// broadcast A[i][k] to all local tasks that belong to this step
-  class LBcastA : public Op<Key<4>, std::tuple<Out<Key<3>, Blk>, Out<Key<4>, Blk>>, LBcastA, Blk, Control> {
+  class LBcastA : public Op<Key<4>, std::tuple<Out<Key<3>, Blk>, Out<Key<4>, Blk>>, LBcastA, const Blk, const Control> {
    public:
-    using baseT = Op<Key<4>, std::tuple<Out<Key<3>, Blk>, Out<Key<4>, Blk>>, LBcastA, Blk, Control>;
+    using baseT = Op<Key<4>, std::tuple<Out<Key<3>, Blk>, Out<Key<4>, Blk>>, LBcastA, const Blk, const Control>;
 
     LBcastA(Edge<Key<4>, Blk> &a_riks, Edge<Key<4>, Control> &ctl_riks, Edge<Key<3>, Blk> &a_ijk,
             std::shared_ptr<const Plan> plan, const Keymap &keymap)
@@ -1188,9 +1188,9 @@ class SpMM {
   };  // class LBcastA
 
   /// broadcast B[k][j] to all procs where A[i][k]
-  class BcastB : public Op<Key<2>, std::tuple<Out<Key<3>, Blk>>, BcastB, Blk> {
+  class BcastB : public Op<Key<2>, std::tuple<Out<Key<3>, Blk>>, BcastB, const Blk> {
    public:
-    using baseT = Op<Key<2>, std::tuple<Out<Key<3>, Blk>>, BcastB, Blk>;
+    using baseT = Op<Key<2>, std::tuple<Out<Key<3>, Blk>>, BcastB, const Blk>;
 
     BcastB(Edge<Key<2>, Blk> &b_mn, Edge<Key<3>, Blk> &b_rkj, std::shared_ptr<const Plan> plan, const Keymap &keymap)
         : baseT(edges(b_mn), edges(b_rkj), "SpMM::BcastB", {"b_mn"}, {"b_rkj"}, keymap), plan_(plan), keymap_(keymap) {}
@@ -1266,9 +1266,9 @@ class SpMM {
   };  // class LStoreB
 
   /// broadcast B[k][j] to all local tasks that belong to this step
-  class LBcastB : public Op<Key<4>, std::tuple<Out<Key<3>, Blk>, Out<Key<4>, Blk>>, LBcastB, Blk, Control> {
+  class LBcastB : public Op<Key<4>, std::tuple<Out<Key<3>, Blk>, Out<Key<4>, Blk>>, LBcastB, const Blk, const Control> {
    public:
-    using baseT = Op<Key<4>, std::tuple<Out<Key<3>, Blk>, Out<Key<4>, Blk>>, LBcastB, Blk, Control>;
+    using baseT = Op<Key<4>, std::tuple<Out<Key<3>, Blk>, Out<Key<4>, Blk>>, LBcastB, const Blk, const Control>;
 
     LBcastB(Edge<Key<4>, Blk> &b_rkjs, Edge<Key<4>, Control> &ctl_rkjs, Edge<Key<3>, Blk> &b_ijk,
             std::shared_ptr<const Plan> plan, const Keymap &keymap)
