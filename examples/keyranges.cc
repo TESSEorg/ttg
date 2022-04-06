9c2dd03c116dcaa6a6c0752c7f9ba52af1d28e77
Joseph Schuchart <schuchart@icl.utk.edu>
2022-04-06 18:11:26 -0400

    Add benchmark for finding a stride in a range of keys
    Signed-off-by: Joseph Schuchart <schuchart@icl.utk.edu>

--- examples/keyranges.cc
+++ examples/keyranges.cc
@@ -1,5 +1,6 @@
 #include <vector>
 #include <chrono>
+#include <unordered_map>
 #include "ttg.h"
 
 /* 1M keys */
@@ -20,6 +21,15 @@ struct Key {
   }
 
   Key& operator+=(const Key& b) {
+    I += b.I;
+    J += b.J;
+    K += b.K;
+    //rehash();
+    return *this;
+  }
+
+  Key operator-(const Key& b) {
+    Key key;
     I += b.I;
     J += b.J;
     K += b.K;
@@ -69,6 +79,15 @@ public:
 };
 
 
+Key operator-(const Key& a, const Key& b) {
+  Key key = a;
+  key.I -= b.I;
+  key.J -= b.J;
+  key.K -= b.K;
+  //rehash();
+  return key;
+}
+
 auto populate_vector() {
   /* vector with enough elements reserved */
   std::vector<Key> vec;
@@ -107,6 +126,96 @@ uint64_t iterate(Range&& vec, Func&& fn, const char* msg) {
   return res;
 }
 
+struct stride_info {
+  Key last_point;
+  Key stride;
+  bool has_stride = false;
+
+  stride_info(Key last_point) : last_point(last_point)
+  { }
+};
+
+template<typename Range, typename Func>
+auto find_stride_map(Range&& range, Func&& fn, const char *msg) {
+  std::chrono::high_resolution_clock::time_point t1, t2;
+  uint64_t duration;
+  std::cout << msg;
+  t1 = std::chrono::high_resolution_clock::now();
+  std::unordered_map<int, stride_info> info;
+  for (int i = 0; i < NUM_ITER_REPS; ++i) {
+    info.clear();
+    for (const auto& v : range) {
+      int proc = fn(v);
+      auto iter = info.find(proc);
+      if (iter != info.end()) {
+        auto stride = v - iter->second.last_point;
+        if (!iter->second.has_stride) {
+          iter->second.stride = stride;
+          iter->second.has_stride = true;
+        } else {
+          /* check if stride is correct */
+          if (iter->second.stride != stride) {
+            std::cout << "STRIDE MISMATCH: " << iter->second.stride << " vs " << stride << " for v " << v << std::endl;
+            return std::unordered_map<int, stride_info>();
+          }
+        }
+        iter->second.last_point = v;
+      } else {
+        /* insert a new element */
+        info.insert(std::make_pair(proc, stride_info(v)));
+      }
+    }
+  }
+  t2 = std::chrono::high_resolution_clock::now();
+  duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+  std::cout << duration/NUM_ITER_REPS << "us" << std::endl;
+  return info;
+}
+
+
+template<typename Range, typename Func>
+auto find_stride_vec(Range&& range, Func&& keymap, const char *msg) {
+  std::chrono::high_resolution_clock::time_point t1, t2;
+  uint64_t duration;
+  Key stride;
+  std::cout << msg;
+  t1 = std::chrono::high_resolution_clock::now();
+  for (int k = 0; k < NUM_ITER_REPS; ++k) {
+    std::vector<int> peers;
+    peers.push_back(keymap(*range.begin())); // mark the first element
+    int i = 0;
+    stride = *range.begin();
+    bool have_stride = false;
+    for (const auto& v : range) {
+      int proc = keymap(v);
+      if (peers[0] == proc) {
+        // we found the beginning of the cycle
+        i = 0;
+        if (!have_stride && i > 0) {
+          stride = v - stride;
+          have_stride = true;
+        }
+      } else if (i > peers.size()-1) {
+        // we found a new peer
+        peers.push_back(proc);
+      } else if (peers[i] != proc) {
+        // STRIDE ERROR?
+        std::cout << "STRIDE_ERROR at " << i << ": expected proc " << peers[i] << " but found " << proc << " for key " << v << std::endl;
+        return false;
+      } else {
+        // All is good, we found the peer we expect
+      }
+      ++i;
+    }
+  }
+  t2 = std::chrono::high_resolution_clock::now();
+  duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+  std::cout << duration/NUM_ITER_REPS << "us" << std::endl;
+
+  std::cout << "Detected stride: " << stride << std::endl;
+
+  return true;
+}
 
 int main(int argc, char **argv)
 {
@@ -122,6 +231,7 @@ int main(int argc, char **argv)
                                   Key(1, 1, 1));
 
   std::cout << "Using vector/range with " << NUM_KEYS << " keys" << std::endl;
+
   std::cout << "Populating vector... ";
   t1 = std::chrono::high_resolution_clock::now();
   std::vector<Key> vec;
@@ -148,6 +258,11 @@ int main(int argc, char **argv)
   iterate(range, fn, "Iterating keyrange (inlined)... ");
 
   iterate(range, stdfn, "Iterating keyrange (not inlined)... ");
+
+  /* find the stride, assuming we have 1024 processes */
+  find_stride_map(range, [](const Key& key){ return key.I % 1024; }, "Finding stride with map... ");
+
+  find_stride_vec(range, [](const Key& key){ return key.I % 1024; }, "Finding stride with vector... ");
 
   ttg::finalize();
 
