db27cc6a7b5368994db0323f7276af57e4b0fb91
Eduard Valeyev <eduard@valeyev.net>
2022-10-17 10:11:07 -0400

    ttg::version uses cmake versions + introduced ttg::git_{description,revision}

--- ttg/ttg/util/version.cc
+++ ttg/ttg/util/version.cc
@@ -2,4 +2,15 @@
 
 namespace ttg {
   std::array<int, 3> version() { return {0, 1, 0}; }
+
+  const char* git_revision() noexcept {
+    static const char revision[] = TTG_GIT_REVISION;
+    return revision;
+  }
+
+  const char* git_description() noexcept {
+    static const char description[] = TTG_GIT_DESCRIPTION;
+    return description;
+  }
+
 }  // namespace ttg
