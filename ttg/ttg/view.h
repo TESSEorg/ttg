65668f4fae53691151dd234afbc540118d299e58
Joseph Schuchart <schuchart@icl.utk.edu>
2022-10-26 15:28:49 -0400

    Provide a scope for views and replace ttg::span with ttg::ViewSpan
    [ttg|std]::span is not a good fit because it carries a size template
    argument that we don't care about but makes handling more difficult.
    Plus, we need to encode the scope for each span.
    
    Three scopes are available:
    1) SyncIn: copy host data to device before invoking the kernel callable.
    2) SyncOut: copy the device data back to the host before invoking the output callable.
    3) Allocate: allocate but do not synchronize in or out.
    
    Both SyncIn and SyncOut will allocate sufficient memory on the device.
    
    Signed-off-by: Joseph Schuchart <schuchart@icl.utk.edu>

--- ttg/ttg/view.h
+++ ttg/ttg/view.h
@@ -6,33 +6,64 @@
 
 namespace ttg {
 
-  namespace detail {
+  enum class ViewScope {
+    Allocate  = 0x0,
+    SyncIn    = 0x1,
+    SyncOut   = 0x2
+  };
 
-    template<typename T>
-    struct typed_iov {
-      T* ptr;
-      std::size_t size;
-    };
+  template<typename T>
+  struct ViewSpan {
 
-    template<typename... Ts>
-    struct typed_iovs {
-      std::tuple<typed_iov<Ts...>> iovs;
-    };
+    using element_type = T;
+    using value_type   = std::remove_cv_t<T>;
 
-  } // namespace detail
+    constexpr ViewSpan() = default;
+    constexpr ViewSpan(T* ptr, std::size_t size, ViewScope scope = ViewScope::SyncIn)
+    : m_ptr(ptr)
+    , m_size(size)
+    , m_scope(scope)
+    { }
 
-  template<typename HostT, typename... DevTs>
+    constexpr std::size_t size() const {
+      return m_size;
+    }
+
+    constexpr T* data() const {
+      return m_ptr;
+    }
+
+    constexpr bool is_allocate() const {
+      return (m_scope == ViewScope::Allocate);
+    }
+
+    constexpr bool is_sync_in() const {
+      return !!(static_cast<int>(m_scope) & static_cast<int>(ViewScope::SyncIn));
+    }
+
+    constexpr bool is_sync_out() const {
+      return !!(static_cast<int>(m_scope) & static_cast<int>(ViewScope::SyncOut));
+    }
+
+    constexpr ViewScope scope() const {
+      return m_scope;
+    }
+
+  private:
+    T *m_ptr = nullptr;
+    std::size_t m_size = 0;
+    ViewScope m_scope = ViewScope::Allocate;
+  };
+
+  template<typename HostT, typename... DevTypeTs>
   struct View {
 
-    using span_tuple_type = std::tuple<ttg::span<DevTs>...>;
+    using span_tuple_type  = std::tuple<ttg::ViewSpan<DevTypeTs>...>;
     using host_type = HostT;
 
-    using view_type = View<HostT, DevTs...>;
+    using view_type = View<HostT, DevTypeTs...>;
 
-    View()
-    : m_obj(nullptr)
-    , m_spans(ttg::span<DevTs>(nullptr, std::size_t{0})...)
-    {}
+    constexpr View() noexcept = default;
 
     View(HostT& obj, span_tuple_type spans)
     : m_obj(&obj)
@@ -61,6 +92,11 @@ namespace ttg {
       return std::get<i>(m_spans).size();
     }
 
+    template<std::size_t i>
+    auto get_span() const {
+      return std::get<i>(m_spans);
+    }
+
     HostT& get_host_object() {
       return *m_obj;
     }
@@ -69,26 +105,24 @@ namespace ttg {
       return *m_obj;
     }
 
+    template<std::size_t i>
+    ViewScope get_scope() const {
+      return std::get<i>(m_spans).scope();
+    }
+
     constexpr static std::size_t size() {
       return std::tuple_size_v<decltype(m_spans)>;
     }
 
   private:
-    HostT* m_obj;
-    span_tuple_type m_spans;
-
+    HostT* m_obj = nullptr;
+    span_tuple_type m_spans{};
   };
 
-  template<typename HostT, typename... Spans>
-  auto make_view(HostT& obj, std::tuple<Spans...> spans) {
-    return View(obj, std::move(spans));
+  template<typename HostT, typename... ViewSpanTs>
+  auto make_view(HostT& obj, ViewSpan<ViewSpanTs>... spans) {
+    return View(obj, std::make_tuple(std::move(spans)...));
   }
-
-  template<typename HostT, typename... Spans>
-  auto new_view(HostT& obj, std::tuple<Spans...> spans) {
-    return View(obj, std::move(spans));
-  }
-
 
 } // namespace ttg
 
