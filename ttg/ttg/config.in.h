a5ae409b05778bda2fb3fae1574a16ee797d9b98
Eduard Valeyev <eduard@valeyev.net>
2022-10-31 10:08:42 -0400

    find <coroutine>

---
+++ ttg/ttg/config.in.h
@@ -0,0 +1,14 @@
+//
+// Created by Eduard Valeyev on 10/31/22.
+//
+
+#ifndef TTG_CONFIG_IN_H
+#define TTG_CONFIG_IN_H
+
+/** the C++ header containing the coroutine API */
+#define TTG_CXX_COROUTINE_HEADER <@CXX_COROUTINE_HEADER@>
+
+/** the C++ namespace containing the coroutine API */
+#define TTG_CXX_COROUTINE_NAMESPACE @CXX_COROUTINE_NAMESPACE@
+
+#endif  // TTG_CONFIG_IN_H
