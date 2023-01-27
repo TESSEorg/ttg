d7893df3d8ad524b8660f66eddf6d214d018b113
Joseph Schuchart <schuchart@icl.utk.edu>
2023-01-27 14:45:56 -0500

    Some more device coroutine examples
    Signed-off-by: Joseph Schuchart <schuchart@icl.utk.edu>

--- tests/unit/device_coro.cc
+++ tests/unit/device_coro.cc
@@ -11,13 +11,16 @@ TEST_CASE("Device", "coro") {
       ttg::View<double, double> view = ttg::make_view(val);
       /* wait for the view to be available on the device */
       co_yield view;
+      // co_yield std::tie(view1, view2);
+      // TTG_WAIT_VIEW(view);
       /* once we're back here the data has been transferred */
       CHECK(view.get_device_ptr() != nullptr);
       CHECK(view.size() == sizeof(val));
       CHECK(&view.get_host_object() == &val);
 
       /* here we suspend to wait for a kernel to complete */
-      co_yield device_op_wait_kernel{};
+      co_yield ttg::device_op_wait_kernel{};
+      // TTG_WAIT_KERNEL();
 
       /* we're back, the kernel executed and we can send */
       if (key < 10) {
@@ -30,4 +33,84 @@ TEST_CASE("Device", "coro") {
     if (ttg::default_execution_context().rank() == 0) tt->invoke(0, 0.0);
     ttg::ttg_fence();
   }
+
+  struct A {
+    int a[10];
+    double b[10];
+  };
+
+
+  SECTION("device_task_struct") {
+    ttg::Edge<int, double> edge;
+    auto fn = [](const int& key, A&& val) -> ttg::device_task {
+      auto view = ttg::make_view(val, {val.a, 10, ttg::ViewScope::SyncIn},
+                                      {val.b, 10, ttg::ViewScope::SyncIn|ttg::ViewScope::SyncOut});
+      /* wait for the view to be available on the device */
+      co_yield view;
+      // co_yield std::tie(view1, view2);
+      // TTG_WAIT_VIEW(view);
+      /* once we're back here the data has been transferred */
+      CHECK(view.get_device_ptr<0>() != nullptr);
+      CHECK(view.size<0>() == 10*sizeof(int));
+      CHECK(&view.get_host_object() == &val);
+
+      // <submit kernel here>
+
+      /* here we suspend to wait for a kernel to complete */
+      co_yield ttg::device_op_wait_kernel{};
+      // TTG_WAIT_KERNEL();
+
+      /* we're back, the kernel executed and we can send */
+      if (key < 10) {
+        ttg::send<0>(key+1, val);
+      }
+    };
+    auto tt = ttg::make_tt(fn, ttg::edges(edge), ttg::edges(edge),
+                           "device_task", {"edge_in"}, {"edge_out"});
+    make_graph_executable(tt);
+    if (ttg::default_execution_context().rank() == 0) tt->invoke(0, 0.0);
+    ttg::ttg_fence();
+  }
+
+
+
+  SECTION("device_task_struct_multi") {
+    ttg::Edge<int, double> edge;
+    auto fn = [](const int& key, A&& val) -> ttg::device_task {
+      auto view = ttg::make_view(val, {val.a, 10},
+                                      {val.b, 10, ttg::ViewScope::SyncOut});
+      /* wait for the view to be available on the device */
+      co_yield view;
+      // co_yield std::tie(view1, view2);
+      // TTG_WAIT_VIEW(view);
+      /* once we're back here the data has been transferred */
+      CHECK(view.get_device_ptr<0>() != nullptr);
+      CHECK(view.size<0>() == 10*sizeof(int));
+      CHECK(&view.get_host_object() == &val);
+
+      // <submit kernel here>
+
+      /* here we suspend to wait for a kernel to complete */
+      co_yield ttg::device_op_wait_kernel{};
+      // TTG_WAIT_KERNEL();
+
+      if (val.b[0] < 100.0) {
+        // <submit another kernel here>
+        view.sync_out<0>();
+        // anything submitted before this point must finish before the data is returned
+        co_yield view;
+      }
+
+      /* we're back, the kernel executed and we can send */
+      if (key < 10) {
+        ttg::send<0>(key+1, val);
+      }
+    };
+    auto tt = ttg::make_tt(fn, ttg::edges(edge), ttg::edges(edge),
+                           "device_task", {"edge_in"}, {"edge_out"});
+    make_graph_executable(tt);
+    if (ttg::default_execution_context().rank() == 0) tt->invoke(0, 0.0);
+    ttg::ttg_fence();
+  }
+
 }
