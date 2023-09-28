3ba4a4f536170d79b1e71158079c7c8105a97fa8
Joseph Schuchart <schuchart@icl.utk.edu>
2023-09-28 16:09:03 +0000

    We need one cublas/hipblas context per device
    Signed-off-by: Joseph Schuchart <schuchart@icl.utk.edu>

--- ttg/ttg/device/cublas_helper.cpp
+++ ttg/ttg/device/cublas_helper.cpp
@@ -4,6 +4,14 @@
 #include <memory>
 #include <stdexcept>
 #include <optional>
+#include <map>
+#ifdef TTG_HAVE_CUDART
+#include <cuda_runtime.h>
+#endif // TTG_HAVE_CUDART
+
+#ifdef TTG_HAVE_HIPBLAS
+#include <hip.h>
+#endif // TTG_HAVE_HIPBLAS
 
 namespace ttg::detail {
 
@@ -11,14 +19,23 @@ namespace ttg::detail {
 /// \brief Returns the cuBLAS handle to be used for launching cuBLAS kernels from the current thread
 /// \return the cuBLAS handle for the current thread
 const cublasHandle_t& cublas_get_handle() {
-  static thread_local std::optional<cublasHandle_t> handle;
-  if (!handle.has_value()) {
-    auto status = cublasCreate_v2(&handle.emplace());
+  static thread_local std::map<int, cublasHandle_t> handles;
+  int device;
+  if (cudaSuccess != cudaGetDevice(&device)){
+      throw std::runtime_error("cudaGetDevice failed");
+  }
+  std::map<int, cublasHandle_t>::iterator it;
+  if ((it = handles.find(device)) == handles.end()){
+    cublasHandle_t handle;
+    auto status = cublasCreate_v2(&handle);
     if (CUBLAS_STATUS_SUCCESS != status) {
       throw std::runtime_error("cublasCreate_v2 failed");
     }
+    auto [iterator, success] = handles.insert({device, handle});
+    it = iterator;
   }
-  return *handle;
+
+  return it->second;
 }
 
 void cublas_set_kernel_stream(cudaStream_t stream) {
@@ -33,14 +50,22 @@ void cublas_set_kernel_stream(cudaStream_t stream) {
 /// \brief Returns the rocBLAS handle to be used for launching rocBLAS kernels from the current thread
 /// \return the rocBLAS handle for the current thread
 const hipblasHandle_t& hipblas_get_handle() {
-  static thread_local std::optional<hipblasHandle_t> handle;
-  if (!handle.has_value()) {
-    hipblasStatus_t status = hipblasCreate(&handle.emplace());
+  static thread_local std::map<int, hipblasHandle_t> handles;
+  int device;
+  if (hipSuccess != hipGetDevice(&device)){
+      throw std::runtime_error("hipGetDevice failed");
+  }
+  std::map<int, hipblasHandle_t>::iterator it;
+  if ((it = handles.find(device)) == handles.end()){
+    hipblasHandle_t handle;
+    auto status = hipblasCreate(&handle);
     if (HIPBLAS_STATUS_SUCCESS != status) {
       throw std::runtime_error("hipblasCreate failed");
     }
+    auto [iterator, success] = handles.insert({device, handle});
+    it = iterator;
   }
-  return *handle;
+  return it->second;
 }
 
 void hipblas_set_kernel_stream(hipStream_t stream) {
