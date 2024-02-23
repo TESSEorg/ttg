ed270da8170a018bf3d9c020fb1c06e20e9519a1
Eduard Valeyev <eduard@valeyev.net>
2024-02-23 15:00:27 -0500

    [dox] new vars config.in.h [skip ci]

--- ttg/ttg/config.in.h
+++ ttg/ttg/config.in.h
@@ -11,19 +11,28 @@
 /** the C++ namespace containing the coroutine API */
 #define TTG_CXX_COROUTINE_NAMESPACE @CXX_COROUTINE_NAMESPACE@
 
+/** whether TTG has CUDA language support */
 #cmakedefine TTG_HAVE_CUDA
 
+/** whether TTG has CUDA runtime support */
 #cmakedefine TTG_HAVE_CUDART
 
+/** whether TTG has HIP support */
 #cmakedefine TTG_HAVE_HIP
 
-#cmakedefine TTG_HAVE_LEVEL_ZERO
-
+/** whether TTG has HIP BLAS library */
 #cmakedefine TTG_HAVE_HIPBLAS
 
+/** whether TTG has Intel Level Zero support */
+#cmakedefine TTG_HAVE_LEVEL_ZERO
+
+/** whether TTG has any device programming model (CUDA/HIP/LEVEL_ZERO) support */
 #cmakedefine TTG_HAVE_DEVICE
 
+/** whether TTG has MPI library */
 #cmakedefine TTG_HAVE_MPI
+
+/** whether TTG has the mpi-ext.h header */
 #cmakedefine TTG_HAVE_MPIEXT
 
 #endif  // TTG_CONFIG_IN_H
