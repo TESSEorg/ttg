84c9c13f930039525a16f2a8c83f61285deb6215
Eduard Valeyev <eduard@valeyev.net>
2024-09-30 21:29:49 -0400

    bump BTAS, MADNESS, and TA tag; most important pull is https://github.com/ValeevGroup/tiledarray/pull/472

--- cmake/modules/ExternalDependenciesVersions.cmake
+++ cmake/modules/ExternalDependenciesVersions.cmake
@@ -3,10 +3,10 @@
 
 set(TTG_TRACKED_VG_CMAKE_KIT_TAG 72bb8f049e68443e817ce7299f0d1dabfaf01b7e)  # provides FindOrFetchLinalgPP and "real" FindOrFetchBoost
 set(TTG_TRACKED_CATCH2_VERSION 3.5.0)
-set(TTG_TRACKED_MADNESS_TAG 96ac90e8f193ccfaf16f346b4652927d2d362e75)
+set(TTG_TRACKED_MADNESS_TAG 95589b0d020a076f93d02eead6da654b23dd3d91)
 set(TTG_TRACKED_PARSEC_TAG 58f8f3089ecad2e8ee50e80a9586e05ce8873b1c)
-set(TTG_TRACKED_BTAS_TAG 4e8f5233aa7881dccdfcc37ce07128833926d3c2)
-set(TTG_TRACKED_TILEDARRAY_TAG 8af44bdff51697de114219fea88db28ca10666e9)
+set(TTG_TRACKED_BTAS_TAG c25b0a11d2a76190bfb13fa72f9e9dc3e57c3c2f)
+set(TTG_TRACKED_TILEDARRAY_TAG ec51edbcafc363c418f5ce235423463d2beb2785)
 
 # need Boost.CallableTraits (header only, part of Boost 1.66 released in Dec 2017) for wrap.h to work
 # BUT if will be building examples, inherit the oldest version from the pickiest Boost consumer (TA and/or BSPMM)
