45af6ec4c65f9c7a137de8ed648992aaeeb1a256
Eduard Valeyev <eduard@valeyev.net>
2024-10-25 09:48:51 -0400

    bump TA tag + associated MAD tag
    most importantly pulls in https://github.com/ValeevGroup/tiledarray/pull/483 which solves linking errors due to missing fmt instantiations

--- cmake/modules/ExternalDependenciesVersions.cmake
+++ cmake/modules/ExternalDependenciesVersions.cmake
@@ -3,10 +3,10 @@
 
 set(TTG_TRACKED_VG_CMAKE_KIT_TAG 72bb8f049e68443e817ce7299f0d1dabfaf01b7e)  # provides FindOrFetchLinalgPP and "real" FindOrFetchBoost
 set(TTG_TRACKED_CATCH2_VERSION 3.5.0)
-set(TTG_TRACKED_MADNESS_TAG 95589b0d020a076f93d02eead6da654b23dd3d91)
+set(TTG_TRACKED_MADNESS_TAG 93a9a5cec2a8fa87fba3afe8056607e6062a9058)
 set(TTG_TRACKED_PARSEC_TAG 58f8f3089ecad2e8ee50e80a9586e05ce8873b1c)
 set(TTG_TRACKED_BTAS_TAG c25b0a11d2a76190bfb13fa72f9e9dc3e57c3c2f)
-set(TTG_TRACKED_TILEDARRAY_TAG ec51edbcafc363c418f5ce235423463d2beb2785)
+set(TTG_TRACKED_TILEDARRAY_TAG 3b4baf7afed4ac7357290f3d90f5d4e3edbc09e0)
 
 # need Boost.CallableTraits (header only, part of Boost 1.66 released in Dec 2017) for wrap.h to work
 # BUT if will be building examples, inherit the oldest version from the pickiest Boost consumer (TA and/or BSPMM)
