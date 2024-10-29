635cdbceef5f0978542584c979bc83744f196834
Joseph Schuchart <joseph.schuchart@stonybrook.edu>
2024-10-29 09:44:28 -0400

    Bump TA tag
    Fixes issues in TA device header.
    
    Signed-off-by: Joseph Schuchart <joseph.schuchart@stonybrook.edu>

--- cmake/modules/ExternalDependenciesVersions.cmake
+++ cmake/modules/ExternalDependenciesVersions.cmake
@@ -6,7 +6,7 @@ set(TTG_TRACKED_CATCH2_VERSION 3.5.0)
 set(TTG_TRACKED_MADNESS_TAG 93a9a5cec2a8fa87fba3afe8056607e6062a9058)
 set(TTG_TRACKED_PARSEC_TAG 58f8f3089ecad2e8ee50e80a9586e05ce8873b1c)
 set(TTG_TRACKED_BTAS_TAG c25b0a11d2a76190bfb13fa72f9e9dc3e57c3c2f)
-set(TTG_TRACKED_TILEDARRAY_TAG 3b4baf7afed4ac7357290f3d90f5d4e3edbc09e0)
+set(TTG_TRACKED_TILEDARRAY_TAG 5944bdba3266a3fa19f1809c8e2accf3dad4d815)
 
 # need Boost.CallableTraits (header only, part of Boost 1.66 released in Dec 2017) for wrap.h to work
 # BUT if will be building examples, inherit the oldest version from the pickiest Boost consumer (TA and/or BSPMM)
