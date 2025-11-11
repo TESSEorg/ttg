67e1684268f5f5dba04dafd7d9ae6469f09a6515
Joseph Schuchart <joseph.schuchart@stonybrook.edu>
2025-11-11 10:33:24 -0500

    Protect coro test with TTG_IMPL_DEVICE_SUPPORT
    Signed-off-by: Joseph Schuchart <joseph.schuchart@stonybrook.edu>

--- tests/unit/device_coro.cc
+++ tests/unit/device_coro.cc
@@ -42,7 +42,7 @@ struct derived_value_t {
   nested_value_t v;
 };
 
-#ifdef TTG_SERIALIZATION_SUPPORTS_MADNESS
+#if defined(TTG_SERIALIZATION_SUPPORTS_MADNESS) && defined(TTG_IMPL_DEVICE_SUPPORT)
 namespace madness {
   namespace archive {
 
